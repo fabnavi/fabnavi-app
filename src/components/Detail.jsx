11f06c40a63caa1906d7271e462bdb7ce9afa209
hrl7 <hrlclb@gmail.com>
2017-04-21 11:38:19 +0900

    chore(all): apply lint:fix

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,11 +1,11 @@
 import React, {Component, PropTypes} from 'react';
 
 export default class Detail extends Component {
-    render(){
-        return (
+  render(){
+    return (
             <div>
                 This is Detail Mode
             </div>
-        )
-    }
+    )
+  }
 }
