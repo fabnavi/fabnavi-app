6dedf13727621b5c28f823433d7dc5a69035f65d
fmsuvM <k222.tkd.fms@gmail.com>
2017-05-31 14:13:52 +0900

    fix code

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,4 +1,4 @@
-import  React, { Component } from 'react';
+import { Component } from 'react';
 import { connect } from 'react-redux';
 import { goBack } from 'react-router-redux';
 import Debug from 'debug';
@@ -6,29 +6,26 @@ import Debug from 'debug';
 const debug = Debug('fabnavi:components:backbutton');
 
 class BackButton extends Component {
-    render(){
-        debug(this.props);
-        // TODO: 後でStyleの変更など  
-        return (
-          <div>
-              <p onClick={this.props.back}>Back Button</p>
-          </div>  
-        );
-    }
+  render() {
+    debug(this.props);
+    return (
+      <div>
+        <p onClick={this.props.back}>Back Button</p>
+      </div>
+    );
+  }
 }
 
-function mapToStateProps(state){
-    debug(state);
-    return state;
+function mapToStateProps(state) {
+  return state;
 }
 
-// これはstore.dispatch()
-function mapDispatchToProps(dispatch){
-    return {
-        back: () => {
-            dispatch(goBack())
-        }
+function mapDispatchToProps(dispatch) {
+  return {
+    back: () => {
+      dispatch(goBack())
     }
+  }
 }
 
 export default connect(mapToStateProps, mapDispatchToProps)(BackButton);
