f1fba45e22dceca6468a496e975f488047abe8a0
fmsuvM <k222.tkd.fms@gmail.com>
2017-05-31 14:15:45 +0900

    fixed spaces

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,11 +1,10 @@
-
 import Debug from 'debug';
 const debug = Debug('fabnavi:reducer');
-const InitialState = "NOBODY";
+const InitialState = 'NOBODY';
 
 export default function userReducer(state = InitialState, action) {
   switch(action.type) {
-    case "LOGGED_IN":
+    case 'LOGGED_IN':
       return action.payload;
     default:
       return state;
