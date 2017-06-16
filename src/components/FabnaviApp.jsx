4a51d82307eb05c9b7cf5b8b8bbe394264da2a5a
fmsuvM <k222.tkd.fms@gmail.com>
2017-06-16 19:22:09 +0900

    Copy from original fabnavi5

--- src/components/FabnaviApp.jsx
+++ src/components/FabnaviApp.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import ReactDOM, { render } from 'react-dom';
 import 'rxjs';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import { createEpicMiddleware } from 'redux-observable';
 import Debug from 'debug';
-import { Route, IndexRoute } from 'react-router';
-import { HashRouter } from 'react-router-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import ProjectList from './ProjectList';
 import ProjectManager from './ProjectManager';
@@ -22,48 +22,55 @@ import WebAPIUtils from '../utils/WebAPIUtils';
 import { changeFrame } from '../actions/frame';
 
 import '../stylesheets/application/application.scss';
-const debug = Debug('fabnavi:jsx:FabnaviApp');
 
+const debug = Debug('fabnavi:jsx:FabnaviApp');
 
 window.api = WebAPIUtils;
 window.addEventListener('DOMContentLoaded', () => {
-  debug('======> Mount App');
-  const url = window.location.href;
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  const store = createStore(reducer,
-    composeEnhancers(applyMiddleware(
-      rootEpics,
-      adjustor)));
+    debug('======> Mount App');
+    const url = window.location.href;
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    const store = createStore(reducer, composeEnhancers(applyMiddleware(rootEpics, adjustor)));
+    
+    const onEnterFrame = frame => (nextState, replace, callback) => {
+        store.dispatch(changeFrame(frame));
+        callback();
+    };
 
-  const onEnterFrame = frame => (nextState, replace, callback) => {
-    store.dispatch(changeFrame(frame));
-    callback();
-  };
+    window.store = store;
+    if (isAuthWindow(url)) {
+        window.opener.postMessage(JSON.stringify(parseAuthInfo(url)), window.location.origin);
+        window.close();
+        return;
+    }
 
-  window.store = store;
-  if(isAuthWindow(url)) {
-    window.opener.postMessage(JSON.stringify(parseAuthInfo(url)), window.location.origin);
-    window.close();
-    return;
-  }
-  api.init(store);
-  ReactDOM.render(
-    <Provider store={store}>
-        <ProjectManager>
-          <ProjectList />
-          </ProjectManager>
-    </Provider>, document.querySelector('#app'));
-  window.addEventListener('keydown', handleKeyDown(store));
-});
+    api.init(store);
+    ReactDOM.render(
+        <Provider store={store}>
+            <Router history={browserHistory}>
+                <Route components={ProjectManager} path="/" onEnter={onEnterFrame('manager')}>
+                    <IndexRoute component={ProjectList} />
+                    <Route component={ProjectList} path="myprojects" />
+                    <Route component={CreateProject} path="create" />
+                    <Route component={EditProject} path="edit/:projectId" onEnter={onEnterFrame('edit')}/>
+                    <Route component={ProjectDetail} path="detail/:projectId" onEnter={onEnterFrame('detail')}/>
+                </Route>
+                <Route components={Player} path="/play/:projectId" onEnter={onEnterFrame('player')} />
+            </Router>
+        </Provider>, document.getElementById('app')
+        );
+    window.addEventListener('keydown', handleKeyDown(store));
+    }
+);
 
 function isAuthWindow(url) {
-  return url.includes('uid') && url.includes('client_id') && url.includes('auth_token');
+    return url.includes('uid') && url.includes('client_id') && url.includes('auth_token');
 }
 
 function parseAuthInfo(url) {
-  return {
-    'Access-Token': url.match(/auth_token=([a-zA-Z0-9\-\_]*)/)[1],
-    'Uid': url.match(/uid=([a-zA-Z0-9\-\_]*)/)[1],
-    'Client': url.match(/client_id=([a-zA-Z0-9\-\_]*)/)[1]
-  };
+    return {
+        'Access-Token': url.match(/auth_token=([a-zA-Z0-9\-\_]*)/)[1],
+        'Uid': url.match(/uid=([a-zA-Z0-9\-\_]*)/)[1],
+        'Client': url.match(/client_id=([a-zA-Z0-9\-\_]*)/)[1]
+    };
 }
