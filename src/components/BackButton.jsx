d266288bce6d9797110aed02301504883f822967
fmsuvM <k222.tkd.fms@gmail.com>
2017-06-18 16:43:04 +0900

    Import backbutton component from backbutton branch

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -6,25 +6,25 @@ import Debug from 'debug';
 const debug = Debug('fabnavi:components:backbutton');
 
 class BackButton extends Component {
-  render() {
-    return (
-      <div>
-        <p onClick={this.props.back}>Back Button</p>
-      </div>
-    );
-  }
+    render() {
+        return (
+          <div>
+            <p onClick={this.props.back}>Back Button</p>
+          </div>
+        );
+    }
 }
 
 function mapToStateProps(state) {
-  return state;
+    return state;
 }
 
 function mapDispatchToProps(dispatch) {
-  return {
-    back: () => {
-      dispatch(goBack())
+    return {
+        back: () => {
+            dispatch(goBack())
+        }
     }
-  }
 }
 
 export default connect(mapToStateProps, mapDispatchToProps)(BackButton);
