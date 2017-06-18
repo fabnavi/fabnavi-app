f45a51de91ebf42a5c8f9cab4e2536f93eb424d0
fmsuvM <k222.tkd.fms@gmail.com>
2017-06-18 16:53:41 +0900

    Import react-router for pushing url & Fix code from old fabnavi5

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 import Debug from 'debug';
+import { memoryHistoy } from 'react-router';
 
 import MainView from '../player/MainView';
 
@@ -10,141 +11,148 @@ const debug = Debug('fabnavi:jsx:Player');
 
 class Player extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.clearCanvas = () => {
-      this.canvas.clear();
-    };
-    this.canvas = null;
-    this.currentImage = null,
-    this.lastPage = 0,
-    this.lastState = '',
-    this.currentState = '';
+    constructor(props) {
+        super(props);
+        this.clearCanvas = () => {
+            this.canvas.clear();
+        };
+        this.canvas = null;
+        this.currentImage = null,
+        this.lastPage = 0,
+        this.lastState = '',
+        this.currentState = '';
 
-    this.updateCanvas = this.updateCanvas.bind(this);
-    this.video = document.createElement('video');
-    this.renderingTimer = null;
-  }
-
-  render() {
-    return (
-      <div>
-        <canvas ref="mainCanvas" />
-      </div>
-    );
-  }
-
-  updateCanvas() {
-    const project = this.props.project;
-    const isValidProject = () => {
-      return typeof project === 'object' && project.content.length !== 0;
-    };
-    if(!isValidProject()) {
-      debug('invalid project data', project);
-      return;
+        this.updateCanvas = this.updateCanvas.bind(this);
+        this.video = document.createElement('video');
+        this.renderingTimer = null;
     }
 
-    if( this.props.contentType === 'movie') {
-      if(this.video.src === '') {
-        this.video.width = window.screen.width;
-        this.video.height = window.screen.height;
-        this.video.src = project.content[0].figure.file.file.url;
-      }
-      if(this.props.isPlaying) {
-        this.renderingTimer = setInterval(() => {
-          this.canvas.render(this.video);
-        }, 30);
-        this.video.play();
-      } else {
-        clearInterval(this.renderingTimer);
-        this.video.pause();
-      }
-      return;
+    render() {
+        return (
+          <div>
+            <canvas ref="mainCanvas" />
+          </div>
+        );
     }
 
-    this.currentState = this.props.mode;
-    if( this.currentState != this.lastState ) {
-      this.canvas.clear();
+    updateCanvas() {
+        const project = this.props.project;
+        const isValidProject = () => {
+            // TODO:  Cannot read property 'content' of null
+            if(project === null){
+                return false;
+            } else {
+                return typeof project === 'object' && project.data.content.length !== 0;
+            }
+        };
+
+        if(!isValidProject()) {
+            debug('invalid project data', project);
+            return;
+        }
+
+        if( this.props.contentType === 'movie') {
+            if(this.video.src === '') {
+                this.video.width = window.screen.width;
+                this.video.height = window.screen.height;
+                this.video.src = project.content[0].figure.file.file.url;
+            }
+            if(this.props.isPlaying) {
+                this.renderingTimer = setInterval(() => {
+                    this.canvas.render(this.video);
+                }, 30);
+                this.video.play();
+            } else {
+                clearInterval(this.renderingTimer);
+                this.video.pause();
+            }
+            return;
+        }
+
+        this.currentState = this.props.mode;
+
+        if( this.currentState != this.lastState ) {
+            this.canvas.clear();
+        }
+        this.lastState = this.currentState;
+
+        const getCurrentImage = () => {
+            return new Promise((resolve, reject) => {
+                if( this.lastPage === this.props.page && this.currentImage != null ) {
+                    resolve(this.currentImage);
+                    // return;
+                }
+
+                const fig = this.props.project.content[this.props.page].figure;
+                this.lastPage = this.props.page;
+                if(fig.hasOwnProperty('clientContent') && fig.clientContent.hasOwnProperty('dfdImage')) {
+                    fig.clientContent.dfdImage
+                    .then(img => {
+                        resolve(img, true);
+                    })
+                    .catch(reject);
+                    return;
+                }
+
+                const img = new Image();
+                this.canvas.redraw();
+                this.canvas.drawWaitingMessage();
+                if(this.lastPage === 0) {
+                    this.canvas.drawInstructionMessage();
+                }
+                img.src = fig.file.file.url;
+                img.onload = (event) => {
+                    resolve(event.target);
+                }
+                img.onerror = reject;
+            });
+        };
+
+        getCurrentImage()
+        .then(img => {
+            this.currentImage = img;
+            this.canvas.draw(this.currentImage, this.props.config);
+
+            if(this.lastPage === 0) {
+                this.canvas.drawInstructionMessage();
+            }
+
+            switch(this.currentState) {
+                case 'calibrateCenter':
+                    this.canvas.drawCalibrateCenterLine();
+                    this.canvas.drawCenterInstruction();
+                    break;
+                case 'calibrateScale':
+                    this.canvas.drawCalibrateScaleLine();
+                    this.canvas.drawScaleInstruction();
+                    break;
+                default:
+                    break;
+            }
+        })
+        .catch(e => {
+            debug('failed to load Image', e);
+        });
     }
-    this.lastState = this.currentState;
 
-    const getCurrentImage = () => {
-      return new Promise((resolve, reject) => {
-        if( this.lastPage === this.props.page && this.currentImage != null ) {
-          resolve(this.currentImage);
-          return;
+    componentWillMount() {
+        if(!this.props.project) {
+            debug('project not loaded!');
+            api.getProject(memoryHistoy.location.pathname.split('/')[2]);
         }
-
-        const fig = this.props.project.content[this.props.page].figure;
-        this.lastPage = this.props.page;
-        if(fig.hasOwnProperty('clientContent') && fig.clientContent.hasOwnProperty('dfdImage')) {
-          fig.clientContent.dfdImage
-          .then(img => {
-            resolve(img, true);
-          })
-          .catch(reject);
-          return;
-        }
-
-        const img = new Image();
-        this.canvas.redraw();
-        this.canvas.drawWaitingMessage();
-        if(this.lastPage === 0) {
-          this.canvas.drawInstructionMessage();
-        }
-        img.src = fig.file.file.url;
-        img.onload = (event) => {
-          resolve(event.target);
-        }
-        img.onerror = reject;
-      });
-    };
-
-    getCurrentImage()
-    .then(img => {
-      this.currentImage = img;
-      this.canvas.draw(this.currentImage, this.props.config);
-
-      if(this.lastPage === 0) {
-        this.canvas.drawInstructionMessage();
-      }
-
-      switch(this.currentState) {
-        case 'calibrateCenter':
-          this.canvas.drawCalibrateCenterLine();
-          this.canvas.drawCenterInstruction();
-          break;
-        case 'calibrateScale':
-          this.canvas.drawCalibrateScaleLine();
-          this.canvas.drawScaleInstruction();
-          break;
-        default:
-          break;
-      }
-    })
-    .catch(e => {
-      debug('failed to load Image', e);
-    });
-  }
-
-  componentWillMount() {
-    if(!this.props.project) {
-      debug('project not loaded!');
-      api.getProject(location.pathname.split('/')[2]);
     }
-  }
 
-  componentDidMount() {
-    this.canvas = new MainView( ReactDOM.findDOMNode(this.refs.mainCanvas));
-  }
+    componentDidMount() {
+        this.canvas = new MainView( ReactDOM.findDOMNode(this.refs.mainCanvas));
+    }
 
-  componentDidUpdate() {
-    this.updateCanvas();
-  }
+    componentDidUpdate() {
+        this.updateCanvas();
+    }
 }
 
 function mapStateToProps(state) {
-  return state.player
+    return state.player
 }
 
 export default connect(mapStateToProps)(Player);
