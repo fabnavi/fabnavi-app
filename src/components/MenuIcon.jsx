ac6c6bb1301df0f8529ae33c2ea0485f44887f4e
fmsuvM <k222.tkd.fms@gmail.com>
2017-06-18 16:52:00 +0900

    Fix code for GitHub Auth & electron

--- src/components/MenuIcon.jsx
+++ src/components/MenuIcon.jsx
@@ -1,100 +1,111 @@
-
 import React from 'react';
 import { connect } from 'react-redux';
 import Debug from 'debug';
 import { browserHistory } from 'react-router';
 
-import { signInFailed, signedIn, signedOut, signingOut } from "../actions/users";
+import { signInFailed, signedIn, signedOut, signingOut } from '../actions/users';
 
 const debug = Debug('fabnavi:jsx:MenuIcon');
+const { remote } = require('electron');
 
 class MenuIcon extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.onClick = () => {
-      if(this.props.hasOwnProperty('to')) {
-        browserHistory.push(this.props.to);
-      }
-      if(this.props.hasOwnProperty('act')) {
-        if(this.props.act === 'sign_in') {
-          this.signIn();
-        } else {
-          this.signOut();
+    constructor(props) {
+        super(props);
+        this.onClick = () => {
+            if(this.props.hasOwnProperty('to')) {
+                browserHistory.push(this.props.to);
+            }
+            if(this.props.hasOwnProperty('act')) {
+                if(this.props.act === 'sign_in') {
+                    this.signIn();
+                } else {
+                    this.signOut();
+                }
+            }
+        };
+
+        this.signIn = () => {
+            const host = 'http://preview.fabnavi.org/';
+            const url = `${host}/auth/github?auth_origin_url=${host}`;
+            const authWindow = new remote.BrowserWindow({
+                modal: true,
+                width: 400,
+                height: 800,
+                webPreferences: {
+                    webSecurity: false,
+                }
+            });
+            authWindow.loadURL(url);
+            const onMessage = (e) => {
+                debug(authWindow.getURL());
+                const url = authWindow.getURL();
+                if(url.includes('uid') && url.includes('client_id') && url.includes('auth_token')){
+                    console.log('action');
+                    this.props.signedIn({
+                    'Access-Token': url.match(/auth_token=([a-zA-Z0-9\-\_]*)/)[1],
+                    'Uid': url.match(/uid=([a-zA-Z0-9\-\_]*)/)[1],
+                    'Client': url.match(/client_id=([a-zA-Z0-9\-\_]*)/)[1]
+                });
+                authWindow.close();
+            }
+            authWindow.once('message', onMessage);
+            authWindow.on('page-title-updated', onMessage);
+            };
         }
-      }
-    };
 
-    this.signIn = () => {
-      const host = window.location.origin;
-      const url = `${host}/auth/github?auth_origin_url=${host}`;
-      window.open(url);
-      const onMessage = (e) => {
-        window.removeEventListener("message", onMessage, false);
+        this.signOut = () => {
+            this.props.signingOut();
+            api.signOut()
+            .then(res => {
+                debug(res);
+                this.props.signedOut();
+            }
+          );
+        };
+    }
 
-        if(e.origin === window.location.origin) {
-          try{
-            debug(">> ", e.data);
-            this.props.signedIn(JSON.parse(e.data));
-          } catch(error) {
-            this.props.signInFailed(error, e);
-          }
-        }
-      };
-      window.addEventListener('message', onMessage);
-    };
-
-    this.signOut = () => {
-      this.props.signingOut();
-      api.signOut()
-      .then(res => {
-        debug(res);
-        this.props.signedOut();
-      });
-    };
-  }
-
-  render() {
-    return (
-      <a className="menu-action nav-action"
-        onClick={this.onClick} >
-        <img src={this.props.src} />
-      </a>
-    );
-  }
+    render() {
+        return (
+          <a className="menu-action nav-action"
+            onClick={this.onClick} >
+            <img src={this.props.src} />
+          </a>
+        );
+    }
 }
 
 function mapStateToProps(state) {
-  return state;
+    return state;
 }
 
 function mapDispatchToProps(dispatch) {
-  return {
-    signedIn: (credential) => {
-      api.saveCredential(credential);
-      dispatch(signedIn(credential));
-    },
-    signingIn: () => {
-      // TODO: (implement) signingInくるくる
-    },
-    signingOut: () => {
-      dispatch(signingOut()); 
-    },
-    signedOut: () => {
-      api.clearCredential();
-      api.clearUserId();
-      dispatch(signedOut());
-    },
-    signInFailed: (error, info) => {
-      const now = new Date();
-      dispatch(signInFailed({
-        message: 'sign in failed. see console',
-        error,
-        info,
-        time: now.toTimeString()
-      }));
+    return {
+        signedIn: (credential) => {
+            api.saveCredential(credential);
+            dispatch(signedIn(credential));
+        },
+        signingIn: () => {
+          // TODO: (implement) signingInくるくる
+        },
+        signingOut: () => {
+            dispatch(signingOut());
+        },
+        signedOut: () => {
+            api.clearCredential();
+            api.clearUserId();
+            dispatch(signedOut());
+        },
+        signInFailed: (error, info) => {
+            const now = new Date();
+            dispatch(signInFailed({
+                message: 'sign in failed. see console',
+                error,
+                info,
+                time: now.toTimeString()
+            }));
+        }
     }
-  }
 }
 
 
