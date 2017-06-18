951054222930f6ff1ca26403068ea66a42f59e79
fmsuvM <k222.tkd.fms@gmail.com>
2017-06-18 16:52:38 +0900

    Fix image file path and Fix code from old fabnavi5 code

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 import Debug from 'debug';
 import { connect } from 'react-redux';
 
@@ -8,35 +9,49 @@ const debug = Debug('fabnavi:jsx:Navigation');
 
 class Navigation extends React.Component {
 
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    console.log("render nav", MenuIcon);
-    const menu = this.props.user.isLoggedIn ? (
-      <div className="menu" >
-        <MenuIcon to="/" src="/images/kaffcop_icon/fab_home.png" />
-        <MenuIcon to="myprojects" src="/images/kaffcop_icon/fab_mypro.png"/>
-        <MenuIcon act="sign_out" src="/images/kaffcop_icon/fab_out.png" />
-      </div>
-    ) : (
-      <div className="menu" >
-        <MenuIcon to="/" src="/images/kaffcop_icon/fab_home.png" />
-        <MenuIcon act="sign_in" src="/images/kaffcop_icon/fab_in.png" />
-      </div>
-    );
+    render() {
+        const menu = this.props.user.isLoggedIn ? (
+        <div className="menu" >
+          <ul>
+            <li><MenuIcon to="/" src="./src/images/fab_home.png" width="200px" height="60px"/></li>
+            <li><MenuIcon to="myprojects" src="./src/images/fab_mypro.png"/></li>
+            <li><MenuIcon act="sign_out" src="./src/images/fab_out.png" /></li>
+          </ul>
+        </div>
+      ) : (
+        <div className="menu" >
+          <li><MenuIcon to="/" src="./src/images/fab_home.png" /></li><MenuIcon to="/" src="./src/images/fab_home.png" />
+          <li><MenuIcon act="sign_in" src="./src/images/fab_in.png" /></li>
+        </div>
+      );
 
-    return (
-      <div className="header">
-      header: あとでなおして
-      </div>
-    );
+        return (
+          <div className="header">
+            <ul className="glonav">
+              <Link className="logo" to="/" >
+                <img src="./src/images/logo.png" />
+              </Link>
+
+              <li>
+                {menu}
+              </li>
+              <li>
+                <Link className="help" to="help">
+                  help
+                </Link>
+              </li>
+            </ul>
+          </div>
+        );
+    }
   }
-}
 
 function mapStateToProps(state) {
-  return state;
+    return state;
 }
 
 export default connect(mapStateToProps)(Navigation);
