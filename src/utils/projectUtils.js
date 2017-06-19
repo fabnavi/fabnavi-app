935be2c4b85c61a1724a1f8f44401f75a4928c8f
fmsuvM <k222.tkd.fms@gmail.com>
2017-06-19 23:40:36 +0900

    No change from previous branch(import-fabnavi5) & Change indent to 4

--- src/utils/projectUtils.js
+++ src/utils/projectUtils.js
@@ -3,68 +3,68 @@ import Debug from 'debug';
 const debug = Debug('fabnavi:utils');
 
 function getUserIconSrc(project) {
-  let src = '';
-  if( project.user.image ) {
-    src = project.user.image;
-  }
-  if(src === '') {
-    src = '/images/kaffcop_icon/user_icon.png'
-  }
-  return src;
+    let src = '';
+    if( project.user.image ) {
+        src = project.user.image;
+    }
+    if(src === '') {
+        src = '/images/kaffcop_icon/user_icon.png'
+    }
+    return src;
 }
 
 function getThumbnailSrc(project) {
-  let src = null;
-  try{
-    if(project.content.length >= 1) {
-      src = project.content[project.content.length - 1].figure.file.file.thumb.url;
+    let src = null;
+    try{
+        if(project.content.length >= 1) {
+            src = project.content[project.content.length - 1].figure.file.file.thumb.url;
+        }
+        if( src == null || src == '' ) {
+            src = './src/images/no_thumbnail.png';
+        }
+        return src;
+    } catch(e) {
+        debug('Catnnot get project thumbnail src ', e);
+        return './src/images/no_thumbnail.png';
     }
-    if( src == null || src == '' ) {
-      src = './src/images/no_thumbnail.png';
-    }
-    return src;
-  } catch(e) {
-    debug('Catnnot get project thumbnail src ', e);
-    return './src/images/no_thumbnail.png';
-  }
 }
 
 function getUploadDate(project) {
-  if(project.hasOwnProperty('created_at')) {
-    return project.created_at.replace(/T.*$/, '').replace(/-/g, ' / ');
-  }
-  debug('invalid project data: ', project);
-  return '';
+    if(project.hasOwnProperty('created_at')) {
+        return project.created_at.replace(/T.*$/, '').replace(/-/g, ' / ');
+    }
+      debug('invalid project data: ', project);
+    return '';
 }
 
 
 function getDescription(project) {
-  if( !project.description ) {
-    return '';
-  }
-  return project.description;
+    if( !project.description ) {
+        return '';
+    }
+    return project.description;
 }
 
 export function trimDescription(desc) {
-  if( desc.length >= 100 ) {
-    return desc.substr(0, 100) + ' . . .';
-  }
-  return desc;
+    if( desc.length >= 100 ) {
+        return desc.substr(0, 100) + ' . . .';
+    }
+    return desc;
 }
 
 export function sanitizeProject(project) {
-  if(!project) {
-    return null;
-  }
-  if(typeof project !== 'object') {
-    // TODO: cannot recover this, they should throw error
-    debug('invalid project data', project);
-    return null;
-  }
-  return Object.assign({}, project, {
-    description: getDescription(project),
-    date: getUploadDate(project),
-    thumbnail: getThumbnailSrc(project),
-    userIcon: getUserIconSrc(project)
-  });
+    if(!project) {
+        return null;
+    }
+    if(typeof project !== 'object') {
+        // TODO: cannot recover this, they should throw error
+        debug('invalid project data', project);
+        return null;
+    }
+    return Object.assign({}, project, {
+        description: getDescription(project),
+        date: getUploadDate(project),
+        thumbnail: getThumbnailSrc(project),
+        userIcon: getUserIconSrc(project)
+    });
 }
