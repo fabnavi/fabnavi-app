a394e898924e9e398822eda7188d6214693bc3e0
fmsuvM <k222.tkd.fms@gmail.com>
2017-06-19 23:30:24 +0900

    Fix code for deleting CHANGE_FRAME & Import react-router-redux module

--- src/components/FabnaviApp.jsx
+++ src/components/FabnaviApp.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'rxjs';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import { createEpicMiddleware } from 'redux-observable';
 import Debug from 'debug';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer, routerMiddleware  } from 'react-router-redux';
+import { createMemoryHistory } from 'history';
 
 import ProjectList from './ProjectList';
 import ProjectManager from './ProjectManager';
@@ -14,7 +15,7 @@ import CreateProject from './CreateProject';
 import EditProject from './EditProject';
 import ProjectDetail from './ProjectDetail';
 
-import reducer from '../reducers/index';
+import reducers from '../reducers/index';
 import adjustor from '../middleware/adjustor';
 import rootEpics from '../middleware/epics/index';
 import { handleKeyDown } from '../actions/KeyActionCreator';
@@ -34,12 +35,27 @@ window.api = WebAPIUtils;
 window.addEventListener('DOMContentLoaded', () => {
     debug('======> Mount App');
     const url = window.location.href;
+    const history = createMemoryHistory();
+    const middleware = routerMiddleware(hashHistory);
     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-    const store = createStore(reducer, composeEnhancers(applyMiddleware(rootEpics, adjustor)));
-    const onEnterFrame = frame => (nextState, replace, callback) => {
-        store.dispatch(changeFrame(frame));
-        callback();
-    };
+    const store = createStore(
+        combineReducers({
+            ...reducers, 
+            routing: routerReducer
+        }),
+        composeEnhancers(applyMiddleware(middleware))
+    );
+    // const history = syncHistoryWithStore(hashHistory, store);
+
+    console.log(('FabnaviApp.jsx'));
+    console.log(store.getState());
+    // const store = createStore(reducer, composeEnhancers(applyMiddleware(rootEpics, adjustor)));
+    // const onEnterFrame = frame => (nextState, replace, callback) => {
+    //     console.log('onEnterFrame is called');
+    //     console.log('frame is ' + frame);
+    //     store.dispatch(changeFrame(frame));
+    //     callback();
+    // };
 
     window.store = store;
     if(isAuthWindow(url)) {
@@ -48,18 +64,18 @@ window.addEventListener('DOMContentLoaded', () => {
         return;
     }
 
-    api.init(store);
+    api.init(store);// 多分これ
     ReactDOM.render(
         <Provider store={store}>
-        <Router history={hashHistory}>
-            <Route components={ProjectManager} path="/" onEnter={onEnterFrame('manager')}>
+        <Router history={history}>
+            <Route components={ProjectManager} path="/" >
                 <IndexRoute component={ProjectList}/>
                 <Route component={ProjectList} path="myprojects"/>
                 <Route component={CreateProject} path="create"/>
                 <Route component={EditProject} path="edit/:projectId"/>
                 <Route component={ProjectDetail} path="detail/:projectId"/>
             </Route>
-            <Route components={Player} path="/play/:projectId" onEnter={onEnterFrame('player')}/>
+            <Route components={Player} path="/play/:projectId" />
         </Router>
     </Provider>, document.getElementById('app'));
     window.addEventListener('keydown', handleKeyDown(store));
