bab7678090c05b31649e1ed188e5aa2760df220f
fmsuvM <k222.tkd.fms@gmail.com>
2017-06-19 23:38:04 +0900

    No change from previous branch(import-fabnavi5) & Change indent to 4

--- src/reducers/frame.js
+++ src/reducers/frame.js
@@ -3,13 +3,15 @@ import Debug from 'debug';
 import Act from '../actions/Types';
 
 const debug = Debug('fabnavi:reducer:frame');
-const initialState = location.pathname.split('/')[1] === 'play' ? 'player' : 'manager';
+const initialState = location.pathname.split('/')[1] === 'play' ? 'player' : 'managers';
 
 export default function frameReducer(state = initialState, action) {
-  switch(action.type) {
-    case Act.CHANGE_FRAME:
-      return action.payload;
-    default:
-      return state;
-  }
+    switch(action.type) {
+        case Act.CHANGE_FRAME:
+            return action.payload;
+        case Act.DETAIL_EXIT:
+            return action.payload;
+        default:
+            return state;
+    }
 }
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,32 +1,32 @@
 import Debug from 'debug';
 
 const debug = Debug('fabnavi:reducer');
-const InitialState = 'NOBODY';
 
 import Act from '../actions/Types';
 
 const initialState = {
-  isLoggedIn: false,
-  credential: {
-    accessToken: '',
-    client: '',
-    uid: '',
-  },
-  id: ''
+    isLoggedIn: false,
+    credential: {
+        accessToken: '',
+        client: '',
+        uid: '',
+    },
+    id: ''
 };
 
 export default function userReducer(state = initialState, action) {
-  switch(action.type) {
-    case Act.SIGNED_IN:
-      debug(action);
-      return Object.assign({}, state, {
-        isLoggedIn: true,
-        credential: action.payload.credential,
-        id: action.payload['id'] ? action.payload.id : ''
-      });
-    case Act.SIGNED_OUT:
-      return initialState;
-    default:
-      return state;
-  }
+    switch(action.type) {
+        case Act.SIGNED_IN:
+            debug(action);
+            return Object.assign({}, state, {
+                isLoggedIn: true,
+                credential: action.payload.credential,
+                id: action.payload['id'] ? action.payload.id : ''
+            }
+            );
+        case Act.SIGNED_OUT:
+            return initialState;
+        default:
+            return state;
+    }
 }
