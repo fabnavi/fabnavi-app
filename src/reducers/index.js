1df39dbd458456d5d94052ae7e55f1be8bf8ce4e
fmsuvM <k222.tkd.fms@gmail.com>
2017-06-19 23:38:33 +0900

    Delete frame.js for deleting CHANGE_FRAME

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,15 +5,13 @@ import Debug from 'debug';
 import player from './player';
 import user from './user';
 import manager from './manager';
-import frame from './frame';
 import errors from './errors';
 
 const debug = Debug('fabnavi:reducer');
 
 export default combineReducers({
-  player,
-  user,
-  manager,
-  frame,
-  errors
+    player,
+    user,
+    manager,
+    errors
 });
