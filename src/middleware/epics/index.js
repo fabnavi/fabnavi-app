7dce01784dc3135a161ae8f33524a5bb1a05708a
fmsuvM <k222.tkd.fms@gmail.com>
2017-06-28 17:42:26 +0900

    Fix code based on new .eslintrc

--- src/middleware/epics/index.js
+++ src/middleware/epics/index.js
@@ -5,12 +5,12 @@ const debug = Debug('fabnavi:epics');
 const signIn = action$ => {
     debug(action$);
     return action$.ofType('SIGN_IN')
-    .do(action => {
-        debug('Sign in', action)
-    })
-    .ignoreElements();
+        .do(action => {
+            debug('Sign in', action)
+        })
+        .ignoreElements();
 }
 
 export default createEpicMiddleware(combineEpics(
-  signIn
+    signIn
 ));
