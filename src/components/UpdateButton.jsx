f09e4440feba1581afe79307ffd94b47298b013e
fmsuvM <k222.tkd.fms@gmail.com>
2017-07-02 15:37:26 +0900

    Add function to reload projects

--- src/components/UpdateButton.jsx
+++ src/components/UpdateButton.jsx
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Debug from 'debug';
 
 const debug = Debug('fabnavi:jsx:UpdateButton');
 
-export default class UpdateButton extends Component {
+class UpdateButton extends Component {
 
     constructor(props) {
         super(props);
+        this.onClick = () => {
+            api.getAllProjects();
+        }
     }
 
     render() {
+        const update = this.props.update ? (
+            <a onClick={this.onClick} className="update">Update</a>
+        ) : (
+            <p className="noUpdate">No Update</p>
+        )
+
         return (
-            <div className="updatebutton">
-                <a>update buttonだよ！</a>
+            <div className="updateButton">
+                {update}
             </div>
         )
     }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if(nextProps.update) {
+            return true;
+        }
+        return false;
+    }
 }
+
+UpdateButton.propTypes = {
+    update: PropTypes.bool
+};
+
+function mapStateToProps(state) {
+    return {
+        update: state.manager.update
+    };
+}
+
+export default connect(mapStateToProps)(UpdateButton);
