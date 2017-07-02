39768e2b5c28d59f2ce285f83a6811c0e9a008fb
satoshicano <satoshicano@gmail.com>
2017-07-02 23:34:09 +0900

    tmp commit

--- src/components/ProjectElement.jsx
+++ src/components/ProjectElement.jsx
@@ -1,25 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
 import Debug from 'debug';
 
 import { sanitizeProject } from '../utils/projectUtils';
 
 const debug = Debug('fabnavi:jsx:ProjectElement');
 
-export default class ProjectElement extends React.Component {
+class ProjectElement extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.menuAction = (actionType) => {
+            this.props.menuAction(this.props.project, actionType);
+        }
+    }
 
     render() {
         const actions = this.props.menuType == 'allProjects' ? (
             <ul className="actions">
                 <li
                     className={`action-box ${this.props.menuIndex == 0 ? 'selected-action' : 'action'}`}
-                    style="border-radius: 5px 5px 0px 0px;" >
+                    style="border-radius: 5px 5px 0px 0px;"
+                    onClick={()=>{
+                        this.menuAction('play')
+                    }} >
                     <div className="menu2">
                         <img src="./src/images/p_play.png" />
                 Play
                     </div>
                 </li>
-                <li className={`action-box ${this.props.menuIndex == 1 ? 'selected-action' : 'action'}`}>
+                <li className={`action-box ${this.props.menuIndex == 1 ? 'selected-action' : 'action'}`}
+                    onClick={()=>{
+                        this.menuAction('detail')
+                    }} >
                     <div className="menu2">
                         <img src="./src/images/p_detail.png" />
                 Detail
@@ -28,25 +43,37 @@ export default class ProjectElement extends React.Component {
             </ul>
         ) : (
             <ul className="actions">
-                <li className={`action-box ${this.props.menuIndex == 0 ? 'selected-action' : 'action'}`}>
+                <li className={`action-box ${this.props.menuIndex == 0 ? 'selected-action' : 'action'}`}
+                    onClick={()=>{
+                        this.menuAction('play')
+                    }} >
                     <div className="menu5">
                         <img src="./src/images/p_play.png" />
                 Play
                     </div>
                 </li>
-                <li className={`action-box ${this.props.menuIndex == 1 ? 'selected-action' : 'action'}`}>
+                <li className={`action-box ${this.props.menuIndex == 1 ? 'selected-action' : 'action'}`}
+                    onClick={()=>{
+                        this.menuAction('detail')
+                    }} >
                     <div className=".menu5">
                         <img src="./src/images/p_detail.png" />
                 Detail
                     </div>
                 </li>
-                <li className={`action-box ${this.props.menuIndex == 2 ? 'selected-action' : 'action'}`}>
+                <li className={`action-box ${this.props.menuIndex == 2 ? 'selected-action' : 'action'}`}
+                    onClick={()=>{
+                        this.menuAction('edit')
+                    }} >
                     <div className="menu5">
                         <img src="./src/images/p_edit.png" />
                 Edit
                     </div>
                 </li>
-                <li className={`action-box ${this.props.menuIndex == 3 ? 'selected-action' : 'action'}`}>
+                <li className={`action-box ${this.props.menuIndex == 3 ? 'selected-action' : 'action'}`}
+                    onClick={()=>{
+                        this.menuAction('delete')
+                    }} >
                     <div className="menu4">
                         <img src="./src/images/p_delete.png" />
                 Delete
@@ -76,7 +103,7 @@ export default class ProjectElement extends React.Component {
                     <div className="description">
                         {project.description}
                     </div>
-                    {this.props.isOpenMenu ? actions : null}
+                    {actions}
                 </div>
             </div>
         );
@@ -88,5 +115,23 @@ ProjectElement.propTypes = {
     menuIndex: PropTypes.number,
     project: PropTypes.object,
     isSelected: PropTypes.bool,
-    isOpenMenu: PropTypes.bool
+    isOpenMenu: PropTypes.bool,
+    menuAction: PropTypes.func
 };
+
+const mapStateToProps = (state) => {
+    return state;
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        menuAction: (project, actionType) => {
+            const selector = {
+                action: actionType
+            }
+            dispatch(push(`/${selector.action}/${project.id}`));
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectElement);
