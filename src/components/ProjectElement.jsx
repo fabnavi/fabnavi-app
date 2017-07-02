3f35007deede6333715a37079cfe71fa05fc2b69
satoshicano <satoshicano@gmail.com>
2017-07-02 23:34:09 +0900

    Feat: enable play project from click
    fix: return only project
    
    必要なもの(project)だけ返すように修正
    
    replace giving props to using state in reducer
    
    projectもreducerに渡したいためpayloadを作成

--- src/components/ProjectElement.jsx
+++ src/components/ProjectElement.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Debug from 'debug';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
+import Debug from 'debug';
 
 import { sanitizeProject } from '../utils/projectUtils';
 import { selectMenuAction } from '../actions/manager';
