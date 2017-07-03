deb6956ea118df4c9460418f76a8bb0ad09a1b43
Katsuya Taguchi <nymph.y.s.0310@ezweb.ne.jp>
2017-07-03 16:13:20 +0900

    help-pageのレイアウト作成

--- src/components/HelpPage.jsx
+++ src/components/HelpPage.jsx
@@ -6,71 +6,83 @@ const debug = Debug('fabnavi:jsx:Help');
 
 export default function HelpPage() { 
     return (
-        <div className="help">
+        <div className="helppage">
           <h2>Help Page</h2>
           <div className="partslist">
             <div className="parts">
-              <img src="./src/images/parts1.png" />
-              FabNavi_CrossPanel
-            </div>
-            <div className="patrs">
-              <img src="./src/images/parts2.png" />
-              FabNavi_LockBar
+              <div className="partstext">
+                FabNavi_CrossPanel
+              </div>
+              <img src="./src/images/parts1.png" /> 
             </div>
             <div className="parts">
+              <div className="partstext">
+                FabNavi_LockBar
+              </div>
+              <img src="./src/images/parts2.png" /> 
+            </div>
+            <div className="parts">
+              <div className="partstext">
+                FabNavi_SidePanel
+              </div>
               <img src="./src/images/parts3.png" />
-              FabNavi_SidePanel
             </div>
             <div className="parts">
+              <div className="partstext">
+                FabNavi_RearPanel
+              </div>
               <img src="./src/images/parts4.png" />
-              FabNavi_RearPanel
             </div>
             <div className="parts">
+              <div className="partstext">
+                FabNavi_UnderRing
+              </div>
               <img src="./src/images/parts5.png" />
-              FabNavi_UnderRing
             </div>
-            <div className="part">
+            <div className="parts">
+              <div className="partstext">
+               FabNavi_TopSpacer
+              </div>
               <img src="./src/images/parts6.png" />
-              FabNavi_TopSpacer
             </div>
           </div>
-          <div className="howmake">
-            How maiking
+          <div className="makelist">
+            <h2>How maiking</h2>
             <div className="make">
-              <div className="howmaketext">
+              <div className="maketext">
                 FabNavi_CrossPanel同士を交差させるように差し込む
-                <img src="./src/images/make1.png" />
               </div>
+              <img src="./src/images/make1.png" />
             </div>
             <div className="make">
-              <div className="howmaketext">
+              <div className="maketext">
                 FabNavi_SidePanelをFabNavi_CrossPanelの端に差し込む
-                <img src="./src/images/make1.png" />
               </div>
+              <img src="./src/images/make2.png" />
             </div>
             <div className="make">
-              <div className="howmaketext">
+              <div className="maketext">
                 FabNavi_RearPanelにFabNavi_UnderRingを差し込む
-                <img src="./src/images/make1.png" />
               </div>
+              <img src="./src/images/make3.png" />
             </div>
             <div className="make">
-              <div className="howmaketext">
+              <div className="maketext">
                 FabNavi_RearPanelをFabNavi_SidePanelとFabNavi_CrossPanelの交差部分に差し込む
-                <img src="./src/images/make1.png" />
               </div>
+              <img src="./src/images/make4.png" />
             </div>
             <div className="make">
-              <div className="howmaketext">
+              <div className="maketext">
                 FabNavi_LockBarをFabNavi_SidePanelに差し込んで留める
-                <img src="./src/images/make1.png" />
               </div>
+              <img src="./src/images/make5.png" />
             </div>
             <div className="make">
-              <div className="howmaketext">
+              <div className="maketext">
                 FabNavi_LockBarにFabNavi_TopSpacerを差し込む
-                <img src="./src/images/make6.png" />
               </div>
+              <img src="./src/images/make6.png" />
             </div>
           </div>
         </div>
