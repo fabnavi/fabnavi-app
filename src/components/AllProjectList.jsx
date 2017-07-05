3dbcb862da84a76e62ff853ceee8d383428fda49
fmsuvM <k222.tkd.fms@gmail.com>
2017-07-05 12:27:44 +0900

    Change: change file name
    ファイル名が余り良くなったので変更．NewProjectList -> AllProjectList

---
+++ src/components/AllProjectList.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Debug from 'debug';
+
+import ProjectElement from './ProjectElement';
+import PageController from './PageController';
+
+const debug = Debug('fabnavi:jsx:AllProjectList');
+
+class AllProjectList extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        // TODO: 表示数を制御する
+        const projects = this.props.projects;
+        const selector = this.props.selector;
+        return (
+            <div className="projects">
+                {projects.map((project, index) =>
+                    <ProjectElement
+                        key={index}
+                        project={project}
+                        isSelected={selector.index == index}
+                        isOpenMenu={selector.index == index && selector.openMenu}
+                        menuIndex={selector.menuIndex}
+                        menuType={selector.menuType} />
+                )}
+                <PageController />
+            </div>
+        )
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.isFetching) {
+            return;
+        }
+        if(this.props.match.path !== nextProps.match.path) {
+            if(nextProps.match.path === 'myprojects') {
+                api.getOwnProjects();
+            } else {
+                api.getAllProjects();
+            }
+        }
+    }
+
+    componentWillMount() {
+        if(this.props.projects.length !== 0) {
+            return;
+        }
+        if(this.props.projects.length === 'myprojects') {
+            api.getOwnProjects();
+        } else {
+            api.getAllProjects();
+        }
+    }
+    
+    // componentWillUpdate(nextProps, nextState) {
+
+    // }
+}
+
+AllProjectList.propTypes = {
+    projects: PropTypes.arrayOf(PropTypes.object),
+    selector: PropTypes.object,
+    isFetching: PropTypes.bool,
+    route: PropTypes.shape({
+        path: PropTypes.string
+    }),
+    initialPage: PropTypes.number,
+    currentPage: PropTypes.number,
+    perPage: PropTypes.number
+}
+
+function mapStateToProps(state) {
+    return {
+        isFetching: state.manager.isFetching,
+        projects: state.manager.projects,
+        selector: state.manager.selector,
+        initialPage: state.manager.initialPage,
+        currentPage: state.manager.currentPage,
+        perPage: state.manager.perPage
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        prevPage: () => {
+            dispatch({
+                type: 'PREV_PROJECTS_PAGE'
+            });
+        },
+        nextPage: () => {
+            dispatch({
+                type: 'NEXT_PROJECTS_PAGE'
+            });
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllProjectList);
