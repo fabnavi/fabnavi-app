457a0cf19cb4c087a4b82270d889f0c38ee7ca93
fmsuvM <k222.tkd.fms@gmail.com>
2017-07-08 16:45:40 +0900

    Add: mouseHover functionとClickした時のfunctionを追加
    状態遷移のためdispatchする関数を作成

--- src/components/ProjectElement.jsx
+++ src/components/ProjectElement.jsx
@@ -1,25 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Debug from 'debug';
+import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
 
 import { sanitizeProject } from '../utils/projectUtils';
 
 const debug = Debug('fabnavi:jsx:ProjectElement');
 
-export default class ProjectElement extends React.Component {
+class ProjectElement extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.over = (projectId) => {
+            this.props.over(projectId)
+        }
+        this.selectMenuAction = (mode) => {
+            this.props.selectMenuAction(this.props.project.id, mode);
+        }
+    }
 
     render() {
         const actions = this.props.menuType == 'allProjects' ? (
             <ul className="actions">
                 <li
                     className={`action-box ${this.props.menuIndex == 0 ? 'selected-action' : 'action'}`}
-                    style="border-radius: 5px 5px 0px 0px;" >
+                    style="border-radius: 5px 5px 0px 0px;"
+                    onClick={() => {
+                        this.selectMenuAction('play')
+                    }}>
                     <div className="menu2">
                         <img src="./src/images/p_play.png" />
                 Play
                     </div>
                 </li>
-                <li className={`action-box ${this.props.menuIndex == 1 ? 'selected-action' : 'action'}`}>
+                <li className={`action-box ${this.props.menuIndex == 1 ? 'selected-action' : 'action'}`}
+                    onClick={() => {
+                        this.selectMenuAction('detail')
+                    }}>
                     <div className="menu2">
                         <img src="./src/images/p_detail.png" />
                 Detail
@@ -28,25 +46,37 @@ export default class ProjectElement extends React.Component {
             </ul>
         ) : (
             <ul className="actions">
-                <li className={`action-box ${this.props.menuIndex == 0 ? 'selected-action' : 'action'}`}>
+                <li className={`action-box ${this.props.menuIndex == 0 ? 'selected-action' : 'action'}`}
+                    onClick={()=>{
+                        this.selectMenuAction('play')
+                    }}>
                     <div className="menu5">
                         <img src="./src/images/p_play.png" />
                 Play
                     </div>
                 </li>
-                <li className={`action-box ${this.props.menuIndex == 1 ? 'selected-action' : 'action'}`}>
+                <li className={`action-box ${this.props.menuIndex == 1 ? 'selected-action' : 'action'}`}
+                    onClick={()=>{
+                        this.selectMenuAction('detail')
+                    }}>
                     <div className=".menu5">
                         <img src="./src/images/p_detail.png" />
                 Detail
                     </div>
                 </li>
-                <li className={`action-box ${this.props.menuIndex == 2 ? 'selected-action' : 'action'}`}>
+                <li className={`action-box ${this.props.menuIndex == 2 ? 'selected-action' : 'action'}`}
+                    onClick={()=>{
+                        this.selectMenuAction('edit')
+                    }}>
                     <div className="menu5">
                         <img src="./src/images/p_edit.png" />
                 Edit
                     </div>
                 </li>
-                <li className={`action-box ${this.props.menuIndex == 3 ? 'selected-action' : 'action'}`}>
+                <li className={`action-box ${this.props.menuIndex == 3 ? 'selected-action' : 'action'}`}
+                    onClick={()=>{
+                        this.selectMenuAction('delete')
+                    }}>
                     <div className="menu4">
                         <img src="./src/images/p_delete.png" />
                 Delete
@@ -57,7 +87,11 @@ export default class ProjectElement extends React.Component {
         const project = sanitizeProject(this.props.project);
 
         return (
-            <div className={`project-box ${this.props.isSelected ? 'selected-project' : ''}`}>
+            <div className={`project-box ${this.props.isSelected ? 'selected-project' : ''}`}
+                onMouseOver={() => {
+                    this.over(project.id)
+                }}
+            >
                 <div className="thumbnail">
                     <img src={project.thumbnail}/>
                 </div>
@@ -88,5 +122,34 @@ ProjectElement.propTypes = {
     menuIndex: PropTypes.number,
     project: PropTypes.object,
     isSelected: PropTypes.bool,
-    isOpenMenu: PropTypes.bool
+    isOpenMenu: PropTypes.bool,
+    selectMenuAction: PropTypes.func,
+    over: PropTypes.func
 };
+
+function mapStateToProps(state) {
+    return state;
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        over: (projectId) => {
+            debug('mouse hover');
+            dispatch({
+                type: 'HOVER_PROJECT',
+                targetProject: projectId
+            })
+        },
+        selectMenuAction: (projectId, mode) => {
+            debug('click menu');
+            dispatch({
+                type: 'SELECT_PROJECT_MENU',
+                targetProject: projectId,
+                mode: mode
+            })
+            dispatch(push(`/${mode}/${projectId}`));
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectElement);
