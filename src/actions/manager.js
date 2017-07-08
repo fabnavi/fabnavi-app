6fd8f6a03a72eeb789a9413f9b2bed7ed574bb0e
fmsuvM <k222.tkd.fms@gmail.com>
2017-07-09 01:47:56 +0900

    Update: update making action method
    文字列を直に打ち込まないように変更

--- src/actions/manager.js
+++ src/actions/manager.js
@@ -1,6 +1,7 @@
 import { createAction } from 'redux-actions';
 
 export const FETCHING_PROJECTS = 'FETCHING_PROJECTS';
+<<<<<<< HEAD
 export const SELECT_PROJECT_MENU = 'SELECT_PROJECT_MENU';
 export const RECEIVE_PROJECTS = 'RECEIVE_PROJECTS';
 export const RECEIVE_PROJECT = 'RECEIVE_PROJECT';
@@ -22,3 +23,23 @@ export function selectMenuAction(projectId, mode) {
         mode: mode
     }
 }
+=======
+export const SELECT_PROJECT = 'SELECT_PROJECT';
+export const SELECT_PROJECT_MENU = 'SELECT_PROJECT_MENU';
+export const FIRE_MENU_ACTION = 'FIRE_MENU_ACTION';
+export const RECEIVE_PROJECTS = 'RECEIVE_PROJECTS';
+export const RECEIVE_PROJECT = 'RECEIVE_PROJECT';
+export const EXIT_DETAIL = 'EXIT_DETAIL';
+export const WILL_UPDATE_PROJECT_LIST = 'WILL_UPDATE_PROJECT_LIST';
+export const BACK_HOME = 'BACK_HOME';
+
+export const fetchingProjects = createAction(FETCHING_PROJECTS);
+export const selectProject = createAction(SELECT_PROJECT);
+export const selectProjectMenu = createAction(SELECT_PROJECT_MENU);
+export const fireMenuAction = createAction(FIRE_MENU_ACTION);
+export const receiveProjects = createAction(RECEIVE_PROJECTS);
+export const receiveProject = createAction(RECEIVE_PROJECT);
+export const exitDetail = createAction(EXIT_DETAIL);
+export const willUpdateProjectList = createAction(WILL_UPDATE_PROJECT_LIST);
+export const backHome = createAction(BACK_HOME);
+>>>>>>> c39bdae... Update: update making action method
