01c0063b59ee07f4d11d435858bdd058abba3485
satoshicano <satoshicano@gmail.com>
2017-07-10 19:52:32 +0900

    Fix: use ProjectMenu Component in ProjectElement

--- src/components/ProjectElement.jsx
+++ src/components/ProjectElement.jsx
@@ -1,87 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { push } from 'react-router-redux';
 import Debug from 'debug';
 
 import { sanitizeProject } from '../utils/projectUtils';
-import { selectMenuAction } from '../actions/manager';
+import ProjectMenu from './ProjectMenu';
 
 const debug = Debug('fabnavi:jsx:ProjectElement');
 
-class ProjectElement extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.selectMenu = (mode) => {
-            this.props.selectMenu(this.props.project.id, mode);
-        }
-    }
+export default class ProjectElement extends React.Component {
 
     render() {
-        const actions = this.props.menuType == 'allProjects' ? (
-            <ul className="actions">
-                <li
-                    className={`action-box ${this.props.menuIndex == 0 ? 'selected-action' : 'action'}`}
-                    style="border-radius: 5px 5px 0px 0px;"
-                    onClick={() => {
-                        this.selectMenu('play')
-                    }}>
-                    <div className="menu2">
-                        <img src="./src/images/p_play.png" />
-                Play
-                    </div>
-                </li>
-                <li className={`action-box ${this.props.menuIndex == 1 ? 'selected-action' : 'action'}`}
-                    onClick={() => {
-                        this.selectMenu('detail')
-                    }}>
-                    <div className="menu2">
-                        <img src="./src/images/p_detail.png" />
-                Detail
-                    </div>
-                </li>
-            </ul>
-        ) : (
-            <ul className="actions">
-                <li className={`action-box ${this.props.menuIndex == 0 ? 'selected-action' : 'action'}`}
-                    onClick={()=>{
-                        this.selectMenu('play')
-                    }}>
-                    <div className="menu5">
-                        <img src="./src/images/p_play.png" />
-                Play
-                    </div>
-                </li>
-                <li className={`action-box ${this.props.menuIndex == 1 ? 'selected-action' : 'action'}`}
-                    onClick={()=>{
-                        this.selectMenu('detail')
-                    }}>
-                    <div className=".menu5">
-                        <img src="./src/images/p_detail.png" />
-                Detail
-                    </div>
-                </li>
-                <li className={`action-box ${this.props.menuIndex == 2 ? 'selected-action' : 'action'}`}
-                    onClick={()=>{
-                        this.selectMenu('edit')
-                    }}>
-                    <div className="menu5">
-                        <img src="./src/images/p_edit.png" />
-                Edit
-                    </div>
-                </li>
-                <li className={`action-box ${this.props.menuIndex == 3 ? 'selected-action' : 'action'}`}
-                    onClick={()=>{
-                        this.selectMenu('delete')
-                    }}>
-                    <div className="menu4">
-                        <img src="./src/images/p_delete.png" />
-                Delete
-                    </div>
-                </li>
-            </ul>);
-
         const project = sanitizeProject(this.props.project);
 
         return (
@@ -104,8 +32,7 @@ class ProjectElement extends React.Component {
                     <div className="description">
                         {project.description}
                     </div>
-                    {/*ここを `ProjectMenu` へ持っていき，compnentを代入する */}
-                    {this.props.isOpenMenu ? actions : null}
+                    <ProjectMenu targetProject={project} menuIndex={0}/>
                 </div>
             </div>
         );
@@ -113,25 +40,7 @@ class ProjectElement extends React.Component {
 }
 
 ProjectElement.propTypes = {
-    menuType: PropTypes.string,
-    menuIndex: PropTypes.number,
     project: PropTypes.object,
     isSelected: PropTypes.bool,
     isOpenMenu: PropTypes.bool,
-    selectMenu: PropTypes.func
 };
-
-function mapStateToProps(state) {
-    return state;
-}
-
-function mapDispatchToProps(dispatch) {
-    return {
-        selectMenu: (projectId, mode) => {
-            selectMenuAction(projectId, mode);
-            dispatch(push(`/${mode}/${projectId}`));
-        }
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ProjectElement);
