35697c5274f6a54046e8bf92d66b1429af1e164b
satoshicano <satoshicano@gmail.com>
2017-07-13 18:33:49 +0900

    fix: indent

--- src/main.js
+++ src/main.js
@@ -4,33 +4,33 @@ const{ app, BrowserWindow, globalShortcut } = require('electron');
 let mainWindow = null;
 
 app.on('window-all-closed', () => {
-  if(process.platform !== 'darwin') {
-    app.quit();
-  }
+    if(process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({
-    frame: true,
-    show: true,
-  });
+    mainWindow = new BrowserWindow({
+        frame: true,
+        show: true,
+    });
 
-  mainWindow.maximize();
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.maximize();
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-  if(process.env.NODE_ENV === 'production') {
-    const loadDevtool = require('electron-load-devtool');
-    loadDevtool(loadDevtool.REACT_DEVELOPER_TOOLS);
-    loadDevtool(loadDevtool.REDUX_DEVTOOLS);
-  }
-
-  const reloadShortcutRegistration = globalShortcut.register('CommandOrControl+R', () => {
-    if(mainWindow) {
-      mainWindow.loadURL('file://' + __dirname + '/index.html');
+    if(process.env.NODE_ENV === 'production') {
+        const loadDevtool = require('electron-load-devtool');
+        loadDevtool(loadDevtool.REACT_DEVELOPER_TOOLS);
+        loadDevtool(loadDevtool.REDUX_DEVTOOLS);
     }
-  });
 
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+    const reloadShortcutRegistration = globalShortcut.register('CommandOrControl+R', () => {
+        if(mainWindow) {
+            mainWindow.loadURL('file://' + __dirname + '/index.html');
+        }
+    });
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
 })
