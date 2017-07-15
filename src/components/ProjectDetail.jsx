c2f0d51226c468f19696acce70bfe5558ecb7dbf
Katsuya Taguchi <nymph.y.s.0310@ezweb.ne.jp>
2017-07-15 18:43:47 +0900

    detail-page内のタイトルをプロジェクト名に変更

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -15,34 +15,36 @@ class ProjectDetail extends React.Component {
     render() {
         const project = sanitizeProject(this.props.targetProject);
         return (
-            <div className="detail-page">
-                <h1>ProjectDetail</h1>
-                <hr className="detail"/>
+            <div>
                 {project ? (
-                    <div className="detail-description">
-                        <div className="project-detail-box">
-                            <div className="thumbnail">
-                                <img src={project.thumbnail}/>
+                    <div className="detail-page">
+                        <h1>{project.name}</h1>
+                        <hr className="detail"/>
+                        <div className="detail-description">
+                            <div className="project-detail-box">
+                                <div className="thumbnail">
+                                    <img src={project.thumbnail}/>
+                                </div>
+                                <h2 className="project-name">
+                                    {project.name}
+                                </h2>
+                                <hr/>
+                                <div className="box">
+                                    <img className="user-icon" src={project.userIcon} />
+                                    <div className="username">
+                                        {project.user.nickname}
+                                    </div>
+                                    <div className="date">
+                                        {project.date}
+                                    </div>
+                                </div>
                             </div>
-                            <h2 className="project-name">
-                                {project.name}
-                            </h2>
-                            <hr/>
-                            <div className="box">
-                                <img className="user-icon" src={project.userIcon} />
-                                <div className="username">
-                                    {project.user.nickname}
-                                </div>
-                                <div className="date">
-                                    {project.date}
-                                </div>
+                            <div className="detail-box">
+                                <h1>Description</h1>
+                                <p>{project.description}</p>
                             </div>
                         </div>
-                        <div className="detail-box">
-                            <h1>Description</h1>
-                            <p>{project.description}</p>
                         </div>
-                    </div>
                 ) : (
                     <div> loading project... </div>
                 )}
