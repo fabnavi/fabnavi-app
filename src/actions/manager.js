24cf8f2fa4df15b265b59b1d8f738bfd5b8bbb9c
satoshicano <satoshicano@gmail.com>
2017-07-19 17:39:52 +0900

    Feat: enable play from click

--- src/actions/manager.js
+++ src/actions/manager.js
@@ -4,7 +4,6 @@ export const FETCHING_PROJECTS = 'FETCHING_PROJECTS';
 export const FETCH_PROJECTS = 'FETCH_PROJECTS';
 export const SELECT_PROJECT_MENU = 'SELECT_PROJECT_MENU';
 export const RECEIVE_PROJECTS = 'RECEIVE_PROJECTS';
-export const RECEIVE_PROJECT = 'RECEIVE_PROJECT';
 export const WILL_UPDATE_PROJECT_LIST = 'WILL_UPDATE_PROJECT_LIST';
 export const CHANGE_PROJECT_LIST_PAGE = 'CHANGE_PROJECT_LIST_PAGE';
 export const UPDATE_PROJECTS = 'UPDATE_PROJECTS';
@@ -12,7 +11,6 @@ export const UPDATE_PROJECTS = 'UPDATE_PROJECTS';
 export const fetchingProjects = createAction(FETCHING_PROJECTS);
 export const selectProjectMenu = createAction(SELECT_PROJECT_MENU);
 export const receiveProjects = createAction(RECEIVE_PROJECTS);
-export const receiveProject = createAction(RECEIVE_PROJECT);
 export const willUpdateProjectList = createAction(WILL_UPDATE_PROJECT_LIST);
 export const changeProjectListPage = createAction(CHANGE_PROJECT_LIST_PAGE, (page) => {
     return page;
@@ -24,14 +22,3 @@ export const fetchProjects = createAction(FETCH_PROJECTS, (page, mode)=> {
     }
 })
 export const updateProjects = createAction(UPDATE_PROJECTS);
-
-/* function each component mapDispatchToProps */
-
-// In ProjectElement.jsx
-export function selectMenuAction(projectId, mode) {
-    return {
-        type: SELECT_PROJECT_MENU,
-        targetProject: projectId,
-        mode: mode
-    }
-}
