404dc623e7ea6b11e4a9f4ba10172187cc292525
fmsuvM <k222.tkd.fms@gmail.com>
2017-07-24 16:15:25 +0900

    Feat(action): add action for showing own projects

--- src/actions/manager.js
+++ src/actions/manager.js
@@ -5,6 +5,7 @@ export const FETCH_PROJECTS = 'FETCH_PROJECTS';
 export const SELECT_PROJECT_MENU = 'SELECT_PROJECT_MENU';
 export const RECEIVE_PROJECT = 'RECEIVE_PROJECT';
 export const RECEIVE_PROJECTS = 'RECEIVE_PROJECTS';
+export const RECEIVE_OWN_PROJECTS = 'RECEIVE_OWN_PROJECTS';
 export const WILL_UPDATE_PROJECT_LIST = 'WILL_UPDATE_PROJECT_LIST';
 export const CHANGE_PROJECT_LIST_PAGE = 'CHANGE_PROJECT_LIST_PAGE';
 export const UPDATE_PROJECT = 'UPDATE_PROJECT';
@@ -14,6 +15,7 @@ export const fetchingProjects = createAction(FETCHING_PROJECTS);
 export const selectProjectMenu = createAction(SELECT_PROJECT_MENU);
 export const receiveProject = createAction(RECEIVE_PROJECT);
 export const receiveProjects = createAction(RECEIVE_PROJECTS);
+export const receiveOwnProjects = createAction(RECEIVE_OWN_PROJECTS);
 export const willUpdateProjectList = createAction(WILL_UPDATE_PROJECT_LIST);
 export const changeProjectListPage = createAction(CHANGE_PROJECT_LIST_PAGE, (page) => {
     return page;
