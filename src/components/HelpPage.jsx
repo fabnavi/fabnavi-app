c448036f0efc512a293d68184714e0aec7f2cb55
fmsuvM <k222.tkd.fms@gmail.com>
2017-07-27 13:27:04 +0900

    temp

--- src/components/HelpPage.jsx
+++ src/components/HelpPage.jsx
@@ -9,34 +9,17 @@ export default class HelpPage extends React.Component {
             <div className="help-page">
                 <h1>Fabnavi Help Page</h1>
                 <hr />
-                <div className="about-application">
-                    <h3 className="contents-title">このアプリの使い方</h3>
-                    <p className="text-contents">
-                        hogehogehogehogehogehogehogehogehoge
-                        hogehogehogehogehogehogehogehogehoge
-                        hogehogehogehogehogehogehogehogehoge
-                        hogehogehogehogehogehogehogehogehoge
-                        hogehogehogehogehogehogehogehogehoge
-                        hogehogehogehogehogehogehogehogehoge
-                        hogehogehogehogehogehogehogehogehoge
-                        hogehogehogehogehogehogehogehogehoge
+                <div className="container">
+                    <h3>Fabnavi Appの使い方</h3>
+                    <p>hogehogehgoehgoehgaslejgha;jglsajogulkjbglskjbaglsijugliualkjgbrlsijbglsiajblgksjbglkjsdblkjbslkjhlvidjsn
+                        gnasoiglaksjnlgkjvnlsdkjrghlsdiurgnl;skjdnglbksjdnrlgisdhr;oeklsdng;slkdrh;bosi;blksn;lgknbvs;okjnbros;ihgpb;osidnr;bsljknb;osidnraohgpo;asj;li
+                        gjsvior;hlhjposu;hgskj;lkrhj;soihrj;glksjn;/girjs;iopjg;spjea@:ou@ewopjq/;klng[ba]@epkg:poegh@apiha:s@eoga[q]@oejg:piqhg@rasipjgbrs
+                        ghiqherg;pqiwj@gp0qwo[kwlqhgpoqsaig:e;lgnqo@ihgpwohgpqig;wqlkrn;qogihweoig@q:porjg@pqwiorvnknbhq@porbmmc0celjwqfgoiqwu2093r7209t8-32jtfa;sopugw0qg-oqsrug0q9ug@]
                     </p>
                 </div>
-
                 <hr />
-
-                <div className="fabnavi-inquiry">
-                    <h3 className="contents-title">お問い合わせ</h3>
-                    <p className="text-contents">
-                        hogehogehogehogehogehogehogehogehoge
-                        hogehogehogehogehogehogehogehogehoge
-                        hogehogehogehogehogehogehogehogehoge
-                        hogehogehogehogehogehogehogehogehoge
-                        hogehogehogehogehogehogehogehogehoge
-                        hogehogehogehogehogehogehogehogehoge
-                        hogehogehogehogehogehogehogehogehoge
-                        hogehogehogehogehogehogehogehogehoge
-                    </p>
+                <div className="container">
+                    <h3>お問い合わせ</h3>
                 </div>
             </div>
         );
