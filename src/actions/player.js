aff7ee2bd910824f82e8745d83466762f72d036c
fmsuvM <k222.tkd.fms@gmail.com>
2017-07-27 11:37:32 +0900

    Refactor(action): fix create actions

--- src/actions/player.js
+++ src/actions/player.js
@@ -1,7 +1,13 @@
 import { createAction } from 'redux-actions';
 
-export const playerChangePage = createAction('PLAYER_CHANGE_PAGE');
-export const updateCalibration = createAction('UPDATE_CALIBRATION');
-export const togglePlaying = createAction('TOGGLE_PLAYING');
-export const playerExit = createAction('PLAYER_EXIT');
-export const playerChangeMode = createAction('PLAYER_CHANGE_MODE');
+export const PLAYER_CHANGE_PAGE = 'PLAYER_CHANGE_PAGE';
+export const UPDATE_CALIBRATION = 'UPDATE_CALIBRATION';
+export const TOGGLE_PLAYING = 'TOGGLE_PLAYING';
+export const PLAYER_EXIT = 'PLAYER_EXIT';
+export const PLAYER_CHANGE_MODE = 'PLAYER_CHANGE_MODE';
+
+export const playerChangePage = createAction(PLAYER_CHANGE_PAGE);
+export const updateCalibration = createAction(UPDATE_CALIBRATION);
+export const togglePlaying = createAction(TOGGLE_PLAYING);
+export const playerExit = createAction(PLAYER_EXIT);
+export const playerChangeMode = createAction(PLAYER_CHANGE_MODE);
