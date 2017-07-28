7991c322f3c80a786fe71d56c139e21637301a38
fmsuvM <k222.tkd.fms@gmail.com>
2017-07-28 22:19:27 +0900

    Fix(help-page): fix list style

--- src/components/HelpPage.jsx
+++ src/components/HelpPage.jsx
@@ -94,10 +94,10 @@ export default class HelpPage extends React.Component {
                                 Project CardはFabaviに投稿されたプロジェクトが入っています．Cardをクリックすると，play〜deleteの操作が表示されます．もう一度クリックすると，このメニューは閉じます．
                             </p>
                             <ul>
-                                <li><p>play:　player modeに遷移します．実際のプロジェクトの内容が表示されます．</p></li>
-                                <li><p>detail:　detail modeに遷移します．detail modeでは詳しい説明をみることが出来ます．</p></li>
-                                <li><p>edit:　edit modeに遷移します．プロジェクトのタイトルや説明などを変えたい場合に使います．自分のプロジェクトを選択した時のみ表示されます．</p></li>
-                                <li><p>delete:　選択したProjectをdeleteします．自分のプロジェクトを選択したときのみ表示されます．</p></li>
+                                <li className="mark-list"><p>play:　player modeに遷移します．実際のプロジェクトの内容が表示されます．</p></li>
+                                <li className="mark-list"><p>detail:　detail modeに遷移します．detail modeでは詳しい説明をみることが出来ます．</p></li>
+                                <li className="mark-list"><p>edit:　edit modeに遷移します．プロジェクトのタイトルや説明などを変えたい場合に使います．自分のプロジェクトを選択した時のみ表示されます．</p></li>
+                                <li className="mark-list"><p>delete:　選択したProjectをdeleteします．自分のプロジェクトを選択したときのみ表示されます．</p></li>
                             </ul>
                         </div>
                     </div>
@@ -109,12 +109,12 @@ export default class HelpPage extends React.Component {
                         <img src="./images/player-mode.png" />
                         <p>Player Modeでは，プロジェクトの画像もしくは動画が再生されます．操作方法は以下の通りです．</p>
                         <ul>
-                            <li className="list-test"><p>プロジェクトが画像の場合:　マウスのクリックで操作します．左クリックで前のページ，右クリックで次のページに進みます．また，キーボードの左右キーでも同じ操作が可能です．</p></li>
-                            <li className="list-test"><p>プロジェクトが動画の場合:　マウスのクリックで操作します．左クリックで動画が再生されます．もう一度クリックすると，再生は止まります．</p></li>
-                            <li className="list-test"><p>Player Modeをやめる場合:　下にスクロールすると，Back Buttonがあります．クリックすると，ホーム画面に遷移します．また，キーボードのESCキーでも同様の操作が可能です．</p></li>
+                            <li className="mark-list"><p>プロジェクトが画像の場合:　マウスのクリックで操作します．左クリックで前のページ，右クリックで次のページに進みます．また，キーボードの左右キーでも同じ操作が可能です．</p></li>
+                            <li className="mark-list"><p>プロジェクトが動画の場合:　マウスのクリックで操作します．左クリックで動画が再生されます．もう一度クリックすると，再生は止まります．</p></li>
+                            <li className="mark-list"><p>Player Modeをやめる場合:　下にスクロールすると，Back Buttonがあります．クリックすると，ホーム画面に遷移します．また，キーボードのESCキーでも同様の操作が可能です．</p></li>
                         </ul>
                         <h6>- Calibration Modeについて -</h6>
-                        <p>申し訳ありません，現在修正中です．実装され次第，アプリケーションが更新されます．</p>
+                        <p>申し訳ありません，現在修正中です．実装され次第アプリケーションが更新されます．</p>
                     </div>
                 </div>
             </div>
