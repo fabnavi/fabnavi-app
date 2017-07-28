dd9780389a581dc1b451fdbefd5cb519b5989135
Zigen <hrlclb@gmail.com>
2017-07-28 22:38:34 +0900

    refactor(help-page): simplify component

--- src/components/HelpPage.jsx
+++ src/components/HelpPage.jsx
@@ -3,6 +3,17 @@ import React from 'react';
 import Debug from 'debug';
 const debug = Debug('fabnavi:jsx:Help');
 
+const MenuDescription = ({ title, src, children }) =>
+    <div className="icon-container">
+        <img src={src} />
+        <div className="container-text">
+            <h5>{title}</h5>
+            <p>
+                {children}
+            </p>
+        </div>
+    </div>;
+
 export default class HelpPage extends React.Component {
     render() {
         return (
@@ -11,77 +22,37 @@ export default class HelpPage extends React.Component {
                 <hr />
                 <div className="container">
                     <h3>Fabnavi Appの使い方</h3>
-                    <div className="icon-container">
-                        <img src="./images/back.png" />
-                        <div className="container-text">
-                            <h5>Back Button</h5>
-                            <p>
-                                一つ前の画面に戻ります．
-                            </p>
-                        </div>
-                    </div>
 
+                    <MenuDescription src="./images/back.png" title="Back Button">
+                        一つ前の画面に戻ります.
+                    </MenuDescription>
                     <hr />
 
-                    <div className="icon-container">
-                        <img src="./images/home.png" />
-                        <div className="container-text">
-                            <h5>Home Button</h5>
-                            <p>
-                                トップページに遷移し，Publicプロジェクトを全て閲覧できます．
-                            </p>
-                        </div>
-                    </div>
-
+                    <MenuDescription src="./images/home.png" title="Home Button">
+                        トップページに遷移し，Publicプロジェクトを全て閲覧できます．
+                    </MenuDescription>
                     <hr />
 
-                    <div className="icon-container">
-                        <img src="./images/myproject.png" />
-                        <div className="container-text">
-                            <h5>My Project Button</h5>
-                            <p>
-                                自分が作成したプロジェクトのみが表示されます．
-                            </p>
-                        </div>
-                    </div>
-
+                    <MenuDescription src="./images/myproject.png" title="Project Button">
+                        自分が作成したプロジェクトのみが表示されます．
+                    </MenuDescription>
                     <hr />
 
-                    <div className="icon-container">
-                        <img src="./images/signin.png" />
-                        <div className="container-text">
-                            <h5>Login Button</h5>
-                            <p>
-                                押すとGitHubアカウントのログイン画面が展開します．ログインすると，全機能を使えるようになります．
-                            </p>
-                        </div>
-                    </div>
-
+                    <MenuDescription src="./images/signin.png" title="Login Button">
+                        押すとGitHubアカウントのログイン画面が展開します．ログインすると，全機能を使えるようになります．
+                    </MenuDescription>
                     <hr />
 
-                    <div className="icon-container">
-                        <img src="./images/help.png" />
-                        <div className="container-text">
-                            <h5>Help Page Button</h5>
-                            <p>
-                                Fabnavi Help Pageに遷移します．
-                            </p>
-                        </div>
-                    </div>
-
+                    <MenuDescription src="./images/help.png" title="Help Page Button">
+                        Fabnavi Help Pageに遷移します．
+                    </MenuDescription>
                     <hr />
 
-                    <div className="icon-container">
-                        <img src="./images/update.png" />
-                        <div className="container-text">
-                            <h5>Update Project Button</h5>
-                            <p>
-                                Fabnavi Serverに新しいプロジェクトがある時，このボタンを押すと表示するプロジェクトが最新の状態になります．
-                            </p>
-                        </div>
-                    </div>
-
+                    <MenuDescription src="./images/update.png" title="Update Project Button" >
+                        Fabnavi Serverに新しいプロジェクトがある時，このボタンを押すと表示するプロジェクトが最新の状態になります．
+                    </MenuDescription>
                     <hr />
+
 
                     <div className="card-container">
                         <div className="card-image-box">
