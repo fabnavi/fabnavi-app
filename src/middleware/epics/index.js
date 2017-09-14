4a67ff5c5cb32cc1a4aefc2a81d1f9380ac2ce59
Satoshi Nakamatsu <satoshicano@gmail.com>
2017-09-15 02:01:57 +0900

    fix: after edit project (#87)
    * fix: jump to detail after edit project
    
    * fix lint

--- src/middleware/epics/index.js
+++ src/middleware/epics/index.js
@@ -33,7 +33,7 @@ const fetchOwnProjectsEpic = (action$) =>
     action$.ofType('@@router/LOCATION_CHANGE')
         .filter(action => action.payload.pathname === '/myprojects')
         .map(action => fetchProjects(action.payload, 'myOwn'));
-;
+
 const fetchProjectEpic = (action$) =>
     action$.ofType('@@router/LOCATION_CHANGE')
         .filter(action => action.payload.pathname !== '/' &&
@@ -44,8 +44,7 @@ const fetchProjectEpic = (action$) =>
             const projectId = action.payload.pathname.match(/\d+/)[0];
             return api.getProject(projectId)
         })
-        .map(({ data }) => receiveProject(data))
-;
+        .map(({ data }) => receiveProject(data));
 
 const fetchProjectsEpic = (action$, store) =>
     action$.ofType(FETCH_PROJECTS)
@@ -53,7 +52,7 @@ const fetchProjectsEpic = (action$, store) =>
         .switchMap(action => {
             const{ mode, page } = action.payload;
             let fetch = api.fetchAllProjects.bind(api);
-            if( mode === 'myOwn'){
+            if( mode === 'myOwn') {
                 fetch = api.fetchOwnProjects.bind(api);
             }
             return Rx.Observable.fromPromise(fetch(page))
@@ -64,11 +63,15 @@ const fetchProjectsEpic = (action$, store) =>
         .map(response => receiveProjects(response))
 ;
 
-const updateProjectEpic = action$ =>
+const updateProjectEpic = (action$, store) =>
     action$.ofType(UPDATE_PROJECT)
         .do(action =>
             api.updateProject(action.payload)
-                .then(res => debug('update success', res))
+                .then(res => {
+                    debug('update success', res.data.id);
+                    store.dispatch(fetchProjects(0, 'all'));
+                    store.dispatch(push(`/detail/${res.data.id}`));
+                })
                 .catch(err => debug(err)))
         .ignoreElements()
 ;
