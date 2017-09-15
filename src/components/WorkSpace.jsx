cf4533ebd8f85ad695edfe1f037ea8ab792d1f01
fmsuvM <k222.tkd.fms@gmail.com>
2017-09-15 16:12:25 +0900

    Feat(WorkSpace): add color picker for changing work space screen color

--- src/components/WorkSpace.jsx
+++ src/components/WorkSpace.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Debug from 'debug';
+import { ChromePicker } from 'react-color';
 
 import BackButton from './BackButton';
-import MainView from '../workspace/MainView';
 
 const debug = Debug('fabnavi:jsx:WorkSpace');
 
@@ -10,29 +10,55 @@ export default class WorkSpace extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            canvas: null,
-            trigger: false
+            spaceColor: '#ffffff'
         }
+        this.handleColorChange = this.handleColorChange.bind(this);
+    }
+
+    componentDidMount() {
+        this.updateCanvas();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        this.updateCanvas();
+    }
+
+    updateCanvas() {
+        const ctx = this.refs.canvas.getContext('2d');
+        const w = screen.width;
+        const h = screen.height;
+        ctx.clearRect(0, 0, w, h);
+        ctx.fillSylte = 'red';
+        this.drawSpace({ ctx, x: 0, y: 0, width: w, height: h });
+    }
+
+    drawSpace(props) {
+        const{ ctx, x, y, width, height } = props;
+        debug('color config', this.state.spaceColor)
+        ctx.fillStyle = this.state.spaceColor;
+        ctx.fillRect(x, y, width, height);
+    }
+
+    handleColorChange(obj) {
+        debug('color num', obj.hex);
+        this.setState({
+            spaceColor: String(obj.hex)
+        })
     }
 
     render() {
         return (
             <div>
-                { this.state.trigger ?
-                    <div>{this.state.canvas}</div> :
-                    <canvas ref="workcvs"></canvas>
-                }
-                <BackButton />
+                <style jsx>{`
+                    .picker-position {
+                    }
+                `}</style>
+                <div>
+                    <canvas ref="canvas" width={1680} height={1050} />
+                    <div className="picker-position"><ChromePicker color={this.state.spaceColor} onChangeComplete={ this.handleColorChange }/></div>
+                    <BackButton />
+                </div>
             </div>
         );
-    }
-
-    componentDidMount() {
-        const canvas = this.refs.workcvs;
-        const cvs = new MainView(canvas);
-        this.setState({
-            canvas: cvs,
-            trigger: true
-        });
     }
 }
