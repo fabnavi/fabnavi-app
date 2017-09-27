0d598852c89f907b1b9e7d23e2bfedff7e18603f
Satoshi Nakamatsu <satoshicano@gmail.com>
2017-09-27 12:47:13 +0900

    feat: electron auto update (#66)
    * add: install auto-updater
    
    * feat: configure windows build
    
    * feat: configure release to github
    
    * feat: enable auto update on app launch

--- src/main.js
+++ src/main.js
@@ -1,8 +1,45 @@
 'use strict';
 const{ app, BrowserWindow, globalShortcut } = require('electron');
+const{ autoUpdater } = require('electron-updater');
 const isDev = require('electron-is-dev');
+const log = require('electron-log');
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
 
 let mainWindow = null;
+
+autoUpdater.on('update-available', (info) => {
+    log.info(info);
+    log.info('Update available.');
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+    log.info(info);
+    log.info('Update downloaded; will install in 5 seconds');
+});
+
+autoUpdater.on('error', (err) => {
+    log.error(err);
+    log.info('Error in auto-updater.');
+});
+
+autoUpdater.on('checking-for-update', () => {
+    log.info('Checking for update...');
+});
+
+autoUpdater.on('update-not-available', (info) => {
+    log.info(info);
+    log.info('Update not available.');
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+    log.info(info);
+    setTimeout(() => {
+        autoUpdater.quitAndInstall();
+    }, 5000);
+});
 
 app.on('window-all-closed', () => {
     if(process.platform !== 'darwin') {
@@ -34,4 +71,8 @@ app.on('ready', () => {
     mainWindow.on('closed', () => {
         mainWindow = null;
     });
-})
+});
+
+app.on('ready', () => {
+    autoUpdater.checkForUpdates();
+});
