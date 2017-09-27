a613960fc40e642d44b3b60b453aeefcc0d01a7c
Satoshi Nakamatsu <satoshicano@gmail.com>
2017-09-27 16:10:18 +0900

    replace to babel-preset-env (#97)
    * add: install babel-preset-env
    
    - remove babel-preset-es2015 and babel-preset-stage3
    
    * fix: remove babel option
    
    * fix: set env to electron
    
    * fix: apply eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,40 @@
 const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = [{
-  entry: path.join(__dirname, 'src/components/FabnaviApp.jsx'),
-  output: {
-    path: path.join(__dirname, 'app'),
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-      test: /.jsx?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-      query: {
-        presets: ['es2015', 'react', 'stage-3'],
-        "plugins": [
-          "transform-object-rest-spread", 
-          "styled-jsx/babel"
-        ]
-      }
-    }, {
-      test: /.scss$/, 
-      use: [{
-        loader: "style-loader"
-      }, {
-        loader: "css-loader"
-      }, {
-        loader: "sass-loader",
-        options: {
-          includePaths: require('bourbon').includePaths.concat(require('bourbon-neat').includePaths)
+    entry: path.join(__dirname, 'src/components/FabnaviApp.jsx'),
+    output: {
+        path: path.join(__dirname, 'app'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /.jsx?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+        }, {
+            test: /.scss$/,
+            use: [{
+                loader: 'style-loader'
+            }, {
+                loader: 'css-loader'
+            }, {
+                loader: 'sass-loader',
+                options: {
+                    includePaths: require('bourbon').includePaths.concat(require('bourbon-neat').includePaths)
+                }
+            }]
+        }]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devtool: 'source-map',
+    externals: [
+        function(context, request, callback) {
+            if(request === 'electron') {
+                return callback(null, 'require(\'' + request + '\')');
+            }
+            return callback();
         }
-      }]
-    }]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  devtool: 'source-map',
-  externals: [
-    function(context, request, callback){
-      if(request === 'electron'){
-        return callback(null, "require('" + request + "')");
-      }
-      return callback();
-    }
-  ]
+    ]
 }]
