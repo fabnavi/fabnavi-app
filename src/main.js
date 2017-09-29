1d0eeb8298e2dcab4740c0e2427028c558be1548
satoshicano <satoshicano@gmail.com>
2017-09-29 11:44:22 +0900

    fix: enable display on macOS

--- src/main.js
+++ src/main.js
@@ -68,6 +68,44 @@ app.on('ready', () => {
         }
     });
 
+    const template = [
+        {
+            label: 'fabnavi',
+            submenu: [
+                {
+                    label: 'Restart',
+                    click: () => mainWindow.reload(),
+                },
+                {
+                    label: 'DevTools',
+                    click: () => mainWindow.webContents.openDevTools({ mode: 'detach' }),
+                },
+                {
+                    label: 'Quit App',
+                    accelerator: 'Command+Q',
+                    click: () => mainWindow.close(),
+                },
+                {
+                    type: 'separator',
+                },
+                {
+                    label: 'About',
+                    click: () => dialog.showMessageBox(mainWindow, {
+                        title: 'fabnavi',
+                        type: 'info',
+                        message: 'fabnavi',
+                        detail: `\nVersion: ${app.getVersion()}\nElectron: ${process.versions.electron}\nRenderer: ${process.versions.chrome}\nNode: ${process.versions.node}\nArchitecture: ${process.platform + ' ' + process.arch}`,
+                        buttons: ['OK'],
+                        noLink: true
+                    })
+                },
+            ],
+        }
+    ]
+
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
+
     mainWindow.on('closed', () => {
         mainWindow = null;
     });
@@ -76,61 +114,3 @@ app.on('ready', () => {
 app.on('ready', () => {
     autoUpdater.checkForUpdates();
 });
-
-const template = [
-    {
-        role: 'about',
-        label: 'about',
-        click () {
-            dialog.showMessageBox(mainWindow, {
-                title: 'fabnavi',
-                type: 'info',
-                message: 'fabnavi',
-                detail: `\nVersion ${app.getVersion()}\nElectron ${process.versions['electron']}\nRenderer ${process.versions['chrome']}\nNode ${process.versions['node']}\nArchitecture ${process.arch}`,
-                buttons: ['OK'],
-                noLink: true
-            });
-        }
-    }
-]
-
-if(process.platform === 'darwin') {
-    template.unshift({
-        label: app.getName(),
-        submenu: [
-            { role: 'about' },
-            { type: 'separator' },
-            { role: 'services', submenu: [] },
-            { type: 'separator' },
-            { role: 'hide' },
-            { role: 'hideothers' },
-            { role: 'unhide' },
-            { type: 'separator' },
-            { role: 'quit' }
-        ]
-    })
-
-    // Edit menu
-    template[1].submenu.push(
-        { type: 'separator' },
-        {
-            label: 'Speech',
-            submenu: [
-                { role: 'startspeaking' },
-                { role: 'stopspeaking' }
-            ]
-        }
-    )
-
-    // Window menu
-    template[3].submenu = [
-        { role: 'close' },
-        { role: 'minimize' },
-        { role: 'zoom' },
-        { type: 'separator' },
-        { role: 'front' }
-    ]
-}
-
-const menu = Menu.buildFromTemplate(template)
-Menu.setApplicationMenu(menu)
