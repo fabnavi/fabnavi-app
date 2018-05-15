213da5f123a7b84a6f2ca95ad538108322ddff29
fmsuvM <k222.tkd.fms@gmail.com>
2018-05-16 07:50:02 +0900

    Feat: add title style based on styled components

---
+++ src/renderer/stylesheets/application/ProjectName.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const ProjectName = styled.h3`
+    width: 160px;
+    height: 40px;
+    margin: 0px;
+    padding: 0px;
+    font-size: 20px;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    margin-top: 13px;
+    margin-left: 10px;
+    margin-bottom: -5px;
+`;
+
+export default ProjectName;
