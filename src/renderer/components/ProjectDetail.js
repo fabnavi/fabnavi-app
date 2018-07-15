7305dd9b0411ff73717e0f286b11df21c119410e
fmsuvM <k222.tkd.fms@gmail.com>
2018-07-15 17:35:05 +0900

    Feat: add ProjectDetail page style
    styled-components仕様にした

--- src/renderer/components/ProjectDetail.js
+++ src/renderer/components/ProjectDetail.js
@@ -7,16 +7,17 @@ import Player from './Player';
 import BackButton from './BackButton';
 
 import { sanitizeProject } from '../utils/projectUtils';
-import { colors, spaces } from '../stylesheets/config.js';
+
+import { PageLayout, ProjectTitle, ProjectDescription } from '../stylesheets/application/ProjectDetail';
+
 const debug = Debug('fabnavi:jsx:ProjectDetail');
 
 class ProjectDetail extends React.Component {
-
     constructor(props) {
         super(props);
         this.showEdit = () => {
             if(this.props.project) {
-                this.props.showEdit(this.props.project.id)
+                this.props.showEdit(this.props.project.id);
             }
         };
     }
@@ -24,48 +25,20 @@ class ProjectDetail extends React.Component {
     render() {
         if(!this.props.project) return <div />;
         const project = sanitizeProject(this.props.project);
-        const isEditable = this.props.userIsAdmin || (project.user.id === this.props.userId);
+        const isEditable = this.props.userIsAdmin || project.user.id === this.props.userId;
         return (
             <div>
-                <style jsx>{`
-                    .detail-page{
-                        width: ${ spaces.solidWidth };
-                        margin-right: auto;
-                        margin-left: auto;
-                    }
-                    .detail-page h1{
-                        font-size: 24px;
-                        color: ${ colors.userNameColor };
-                    }
-                    p {
-                        margin-left: 10px;
-                        color: black;
-                        font-size: 20px;
-                    }
-                    .project-name {
-                        font-size:17pt;
-                        margin: 5px auto;
-                        height:30px;
-                        text-align: center;
-                    }
-                    .detail-description {
-                        display: flex;
-                    }
-                `}</style>
                 {project ? (
-                    <div className="detail-page">
+                    <PageLayout>
                         <Player />
-                        <h1>{project.name}</h1>
+                        <ProjectTitle>{project.name}</ProjectTitle>
                         <hr />
-                        <div className="detail-description">
-                            <div>
-                                <h1>Description</h1>
-                                <p>{project.description}</p>
-                            </div>
+                        <div>
+                            <ProjectDescription>{project.description}</ProjectDescription>
                         </div>
                         <BackButton />
-                        {isEditable ? <EditButton handleClick={this.showEdit} /> : null }
-                    </div>
+                        {isEditable ? <EditButton handleClick={this.showEdit} /> : null}
+                    </PageLayout>
                 ) : (
                     <div> loading project... </div>
                 )}
@@ -75,37 +48,29 @@ class ProjectDetail extends React.Component {
 }
 
 const EditButton = ({ handleClick }) => {
-    return (
-        <div onClick={ () => handleClick() }>
-          Edit Project
-        </div>
-    )
-}
+    return <div onClick={() => handleClick()}>Edit Project</div>;
+};
 
 ProjectDetail.propTypes = {
     project: PropTypes.object,
-    userId: PropTypes.oneOfType([
-        PropTypes.number,
-        PropTypes.string
-    ]),
+    userId: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
     showEdit: PropTypes.func,
-    userIsAdmin: PropTypes.bool,
+    userIsAdmin: PropTypes.bool
 };
 
-const mapStateToProps = (state) => (
-    {
-        project: state.manager.targetProject,
-        userId: state.user.id,
-        userIsAdmin: state.user.isAdmin,
-    }
-);
+const mapStateToProps = state => ({
+    project: state.manager.targetProject,
+    userId: state.user.id,
+    userIsAdmin: state.user.isAdmin
+});
 
-const mapDispatchToProps = (dispatch) => (
-    {
-        showEdit: (projectId) => {
-            dispatch(push(`/edit/${projectId}`));
-        }
+const mapDispatchToProps = dispatch => ({
+    showEdit: projectId => {
+        dispatch(push(`/edit/${projectId}`));
     }
-)
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(ProjectDetail);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(ProjectDetail);
---
+++ src/renderer/stylesheets/application/ProjectDetail.js
@@ -0,0 +1,18 @@
+import styled, { css } from 'styled-components';
+
+export const PageLayout = styled.div`
+    width: 1200px;
+    margin-right: auto;
+    margin-left: auto;
+`;
+
+export const ProjectTitle = styled.h1`
+    font-size: 24px;
+    color: #323232;
+`;
+
+export const ProjectDescription = styled.p`
+    margin-left: 10px;
+    color: black;
+    font-size: 20px;
+`;
