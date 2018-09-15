ea5bb367b02abb5a951c687c11776da1f230eb01
fmsuvM <k222.tkd.fms@gmail.com>
2018-09-15 13:26:07 +0900

    Fix: fix BackButton Style

--- src/renderer/stylesheets/application/BackButton.js
+++ src/renderer/stylesheets/application/BackButton.js
@@ -3,7 +3,9 @@ import styled, { css } from 'styled-components';
 export const BuckButtonStyle = styled.img`
     width: 50px;
     height: 50px;
-    margin: 10px 0 0 0;
+    margin: 0;
+    margin-top: 25px;
+    margin-right: 20px;
 
     &:hover {
         cursor: pointer;
