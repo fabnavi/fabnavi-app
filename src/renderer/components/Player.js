2cbf589f80b37768b9c8a5e09b76ab52bc7f8056
fmsuvM <k222.tkd.fms@gmail.com>
2018-09-21 05:29:13 +0900

    Fix: ImagePlayerのサイズを変更

--- src/renderer/components/Player.js
+++ src/renderer/components/Player.js
@@ -91,8 +91,8 @@ export class Player extends React.Component {
                                 } :
                                 {
                                     display: 'table-cell',
-                                    width: '1280px',
-                                    height: '640px'
+                                    width: '770px',
+                                    height: '505px'
                                 }
                         }
                         ref={this.setCanvasElement}
