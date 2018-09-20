34dddec7ddd672d1dcceb67a9df4c754a96a741e
fmsuvM <k222.tkd.fms@gmail.com>
2018-09-20 17:38:39 +0900

    Feat: カードをクリックした時の挙動を変更
    カードをクリックしたらそのままProjectShowに飛ぶようにした

--- src/renderer/components/ProjectCard.js
+++ src/renderer/components/ProjectCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Debug from 'debug';
+import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
 
 import { sanitizeProject } from '../utils/projectUtils';
 const debug = Debug('fabnavi:jsx:ProjectCard');
@@ -14,29 +16,25 @@ import {
     ProjectIcon,
     CardBorder,
     ProjectBox,
-    ProjectDescription,
-    ProjectMenu,
-    MenuColmun
+    ProjectDescription
 } from '../stylesheets/application/Card';
 
-export default class ProjectCard extends React.Component {
+export class ProjectCard extends React.Component {
     constructor(props) {
         super(props);
-        this.selectItem = mode => () => {
-            this.props.selectMenuItem(props.id, mode);
+        this.toProjectShow = () => {
+            this.props.toProjectShow(this.props.id);
         };
     }
 
     render() {
         const project = sanitizeProject(this.props);
-        const isSelected = this.props.selectedId === this.props.id;
-        const isOwn = project.user.id === this.props.currentUserId;
         const projectType =
             typeof project.content[0] === 'undefined' ? 'Photo' : project.content[0].type.split('::')[1];
 
         return (
             <div>
-                <ProjectFrame onClick={this.props.toggleMenu(this.props.id)} selected={isSelected}>
+                <ProjectFrame onClick={this.toProjectShow}>
                     <ProjectThumbnail>
                         <img src={project.thumbnail} />
                     </ProjectThumbnail>
@@ -58,7 +56,6 @@ export default class ProjectCard extends React.Component {
                         ) : (
                             <ProjectDescription>{project.description}</ProjectDescription>
                         )}
-                        {isSelected ? <Menu isOwn={isOwn} selectItem={this.selectItem} /> : null}
                     </ProjectBox>
                 </ProjectFrame>
             </div>
@@ -66,37 +63,22 @@ export default class ProjectCard extends React.Component {
     }
 }
 
-// TODO: remove Menu and MenuItem
-const Menu = ({ isOwn, selectItem }) => {
-    return (
-        <ProjectMenu>
-            {isOwn ? (
-                <div>
-                    <MenuItem actionName="play" onClick={selectItem} />
-                    <MenuItem actionName="detail" onClick={selectItem} />
-                    <MenuItem actionName="edit" onClick={selectItem} />
-                    <MenuItem actionName="delete" onClick={selectItem} />
-                </div>
-            ) : (
-                <div>
-                    <MenuItem actionName="play" onClick={selectItem} />
-                    <MenuItem actionName="detail" onClick={selectItem} />
-                </div>
-            )}
-        </ProjectMenu>
-    );
-};
-
-const MenuItem = ({ actionName, onClick }) => (
-    <MenuColmun onClick={onClick(actionName)}>
-        <img src={`${assetsPath}/images/p_${actionName}.png`} />
-        <span> {actionName} </span>
-    </MenuColmun>
-);
-
 ProjectCard.propTypes = {
     content: PropTypes.arrayOf(PropTypes.object),
     selectMenuItem: PropTypes.func,
     toggleMenu: PropTypes.func,
-    selectedId: PropTypes.number
+    selectedId: PropTypes.number,
+    toProjectShow: PropTypes.func
 };
+
+const mapDispatchToProps = dispatch => ({
+    toProjectShow: projectId => {
+        debug('hogehoge: ', projectId);
+        dispatch(push(`/detail/${projectId}`));
+    }
+});
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(ProjectCard);
