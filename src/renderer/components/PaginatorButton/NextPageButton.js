cd1e596e211e4462108c561f40ccdd2a571c6068
fmsuvM <k222.tkd.fms@gmail.com>
2018-09-22 19:01:49 +0900

    Fix: 以前の処理をコピー
    しかしコメントアウトしないとコンパイルされない

--- src/renderer/components/PaginatorButton/NextPageButton.js
+++ src/renderer/components/PaginatorButton/NextPageButton.js
@@ -19,17 +19,46 @@ class NextPageButton extends React.Component {
     }
 
     render() {
-        return <StyledPaginatorButton onClick={this.nextPage} src={`${assetsPath}/images/NextButton.png`} next />;
+        // 以前とは違うProps なので，コメントアウトしないとコンパイルされません
+        // const{ filter, isFetching, maxPage, perPage, currentPage, currentUserId } = this.props;
+        // const contents = this.props.contents.allIds
+        //     .filter(id => {
+        //         switch(filter) {
+        //             case 'all':
+        //                 return true;
+        //             case 'myOwn':
+        //                 return this.props.contents.byId[id].user.id == currentUserId;
+        //             default:
+        //                 debug(`invalid state.manager.filter: ${filter}, check state, reducer and actionCreator`);
+        //                 return false;
+        //         }
+        //     })
+        //     .slice(currentPage * perPage, (currentPage + 1) * perPage)
+        //     .map(id => this.props.contents.byId[id]);
+        // TODO: isEndのロジックがおかしいので修正
+        // const isEnd = contents.length !== perPage;
+        return (
+            <div>
+                {/* {!isEnd && (
+                    <StyledPaginatorButton next onClick={this.nextPage} src={`${assetsPath}/images/NextButton.png`} />
+                )} */}
                 <StyledPaginatorButton next onClick={this.nextPage} src={`${assetsPath}/images/NextButton.png`} />
+            </div>
+        )
     }
 }
 
 NextPageButton.propTypes = {
+    projects: PropTypes.shape({
+        byId: PropTypes.object,
+        allIds: PropTypes.arrayOf(PropTypes.number)
+    }),
     nextPage: PropTypes.func,
     currentPage: PropTypes.number
 };
 
 const mapStateToProps = state => ({
+    projects: state.manager.projects,
     currentPage: state.manager.currentPage
 });
 
