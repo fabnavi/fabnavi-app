d78a96165b5245add09a48608d5e4aaf88d65fa4
fmsuvM <k222.tkd.fms@gmail.com>
2018-09-23 12:51:21 +0900

    Feat: Paginator Buttonが消えたので位置を最終調整

--- src/renderer/stylesheets/application/ProjectShow/ProjectSettings.js
+++ src/renderer/stylesheets/application/ProjectShow/ProjectSettings.js
@@ -5,6 +5,7 @@ import { buttonProperties } from '../../common/buttonProperties';
 export const StyledEditButton = styled.button`
     font-size: 20px;
     border: none;
+    margin-right: 50px;
     ${buttonProperties({
         width: 100,
         height: 50,
@@ -17,6 +18,7 @@ export const StyledDeleteButton = styled.button`
     font-size: 20px;
     border: 1px solid #555;
     margin-top: 20px;
+    margin-right: 50px;
     ${buttonProperties({
         width: 100,
         height: 50,
