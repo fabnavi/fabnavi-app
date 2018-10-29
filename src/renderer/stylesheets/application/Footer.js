ea322b64785acb6d95661c94521a76602d900ae5
fmsuvM <k222.tkd.fms@gmail.com>
2018-10-29 13:45:12 +0900

    [WIP]

--- src/renderer/stylesheets/application/Footer.js
+++ src/renderer/stylesheets/application/Footer.js
@@ -2,10 +2,11 @@ import styled from 'styled-components';
 
 export const FooterStyle = styled.footer`
     background-color: white;
-    height: 60px;
+    /* height: 60px; */
     text-align: left;
-    margin-top: 30px;
+    margin-top: 80px;
     margin-left: 55px;
+    margin-bottom: 25px;
 `;
 
 export const FooterBorder = styled.hr`
