13f238bbe08d0ef6650cc23caeb9e62f49bd2e93
fmsuvM <k222.tkd.fms@gmail.com>
2018-11-06 16:45:09 +0900

    Feat: update footer style
    XDのpixel sizeに合わせてfooterの位置を調整

--- src/renderer/stylesheets/application/Footer.js
+++ src/renderer/stylesheets/application/Footer.js
@@ -4,9 +4,9 @@ export const FooterStyle = styled.footer`
     background-color: white;
     /* height: 60px; */
     text-align: left;
-    margin-top: 80px;
+    margin-top: 78px;
     margin-left: 55px;
-    margin-bottom: 25px;
+    margin-bottom: 40px;
 `;
 
 export const FooterBorder = styled.hr`
